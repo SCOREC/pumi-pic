5085cf0e7f88b6dbf5be7ef14f79927038a5bcf3
Cameron Smith <smithc11@rpi.edu>
2018-10-24 16:46:08 -0500

    kokkos array push compiles

--- particle_structs/Push.cpp
+++ particle_structs/Push.cpp
@@ -1,4 +1,5 @@
 #include "Push.h"
+#include <Kokkos_Core.hpp>
 
 void push_array(int np, double* xs, double* ys, double* zs, double distance, double dx,
                 double dy, double dz, double* new_xs, double* new_ys, double* new_zs) {
@@ -8,6 +9,65 @@ void push_array(int np, double* xs, double* ys, double* zs, double distance, dou
     new_zs[i] = zs[i] + distance * dz;
   }
 }
+
+#ifdef KOKKOS_ENABLED
+typedef double fp_t;
+typedef Kokkos::DefaultExecutionSpace exe_space;
+typedef Kokkos::View<fp_t*, exe_space::device_type> kkFpView;
+
+/** \brief helper function to transfer a host array to a device view
+ */
+void hostToDevice(kkFpView d, fp_t* h) {
+  kkFpView::HostMirror hv = Kokkos::create_mirror_view(d);
+  for (size_t i=0; i<hv.size(); ++i)
+    hv(i) = h[i];
+  Kokkos::deep_copy(d,hv);
+}
+/** \brief helper function to transfer a device view to a host array
+ */
+void deviceToHost(kkFpView d, fp_t* h) {
+  kkFpView::HostMirror hv = Kokkos::create_mirror_view(d);
+  Kokkos::deep_copy(hv,d);
+  for(size_t i=0; i<hv.size(); ++i)
+    h[i] = hv(i);
+}
+
+void push_array_kk(int np, double* xs, double* ys, double* zs, double distance, double dx,
+                double dy, double dz, double* new_xs, double* new_ys, double* new_zs) {
+  kkFpView xs_d("xs_d", np);
+  hostToDevice(xs_d, xs);
+
+  kkFpView ys_d("ys_d", np);
+  hostToDevice(ys_d, ys);
+
+  kkFpView zs_d("zs_d", np);
+  hostToDevice(zs_d, zs);
+
+  kkFpView new_xs_d("new_xs_d", np);
+  hostToDevice(new_xs_d, new_xs);
+
+  kkFpView new_ys_d("new_ys_d", np);
+  hostToDevice(new_ys_d, new_ys);
+
+  kkFpView new_zs_d("new_zs_d", np);
+  hostToDevice(new_zs_d, new_zs);
+
+  fp_t disp[4] = {distance,dx,dy,dz};
+  kkFpView disp_d("direction_d", 4);
+  hostToDevice(disp_d, disp);
+
+  #if defined(KOKKOS_ENABLE_CXX11_DISPATCH_LAMBDA)
+  Kokkos::parallel_for (np, KOKKOS_LAMBDA (const int i) {
+      // Acesss the View just like a Fortran array.  The layout depends
+      // on the View's memory space, so don't rely on the View's
+      // physical memory layout unless you know what you're doing.
+      new_xs_d(i) = xs_d(i) + disp_d(0) * disp_d(1);
+      new_ys_d(i) = ys_d(i) + disp_d(0) * disp_d(2);
+      new_zs_d(i) = zs_d(i) + disp_d(0) * disp_d(3);
+    });
+  #endif
+}
+#endif //kokkos enabled
 
 void push_scs(SellCSigma* scs, double* xs, double* ys, double* zs, double distance, double dx,
               double dy, double dz, double* new_xs, double* new_ys, double* new_zs) {
---
+++ particle_structs/pskk.cpp
@@ -0,0 +1,110 @@
+#include <stdio.h>
+#include <cstdlib>
+#include <vector>
+#include "SellCSigma.h"
+#include "Distribute.h"
+#include "Push.h"
+#include <math.h>
+#include <time.h>
+#include <cassert>
+
+#include <Kokkos_Core.hpp>
+
+double randD(double fMin, double fMax)
+{
+    double f = (double)rand() / RAND_MAX;
+    return fMin + f * (fMax - fMin);
+}
+int main(int argc, char* argv[]) {
+  Kokkos::initialize(argc,argv);
+  srand(time(NULL));
+  if (argc != 6) {
+    printf("Usage: %s <number of elements> <number of particles> <distribution strategy (0-3)> "
+           "<C> <sigma>\n", argv[0]);
+    return 1;
+  }
+  int ne = atoi(argv[1]);
+  int np = atoi(argv[2]);
+  int strat = atoi(argv[3]);
+
+  //Distribute particles to 'elements'
+  int* ptcls_per_elem = new int[ne];
+  std::vector<int>* ids = new std::vector<int>[ne];
+  if (!distribute_particles(ne,np,strat,ptcls_per_elem, ids)) {
+    return 1;
+  }
+
+#ifdef DEBUG
+  int ps = 0;
+  printf("Particle Distribution\n");
+  for (int i = 0;i < ne; ++i) {
+    printf("Element %d has %d particles:",i, ptcls_per_elem[i]);
+    ps += ptcls_per_elem[i];
+    for (int j = 0; j < ptcls_per_elem[i]; ++j) {
+      printf(" %d",ids[i][j]);
+      assert(ids[i][j] >= 0 && ids[i][j] < np);
+    }
+    printf("\n");
+  }
+  assert(ps == np);
+#endif
+
+  //Create the SellCSigma for particles
+  int C = atoi(argv[4]);
+  int sigma = atoi(argv[5]);
+  SellCSigma* scs = new SellCSigma(C, sigma, ne, np, ptcls_per_elem,ids);
+
+  //Create Coordinates
+  double* xs = new double[np];
+  double* ys = new double[np];
+  double* zs = new double[np];
+  for (int i = 0; i < np; ++i) {
+    xs[i] = 0.125;
+    ys[i] = 5;
+    zs[i] = M_PI;
+  }
+
+  //Push the particles
+  double distance = M_E;
+  double dx = randD(-10,10);
+  double dy = randD(-10,10);
+  double dz = randD(-10,10);
+  double length = sqrt(dx * dx + dy * dy + dz * dz);
+  dx /= length;
+  dy /= length;
+  dz /= length;
+
+  double* new_xs1 = new double[np];
+  double* new_ys1 = new double[np];
+  double* new_zs1 = new double[np];
+  push_array(np, xs, ys, zs, distance, dx, dy, dz, new_xs1, new_ys1, new_zs1);
+
+  double* new_xs2 = new double[np];
+  double* new_ys2 = new double[np];
+  double* new_zs2 = new double[np];
+  push_scs(scs, xs, ys, zs, distance, dx, dy, dz, new_xs2, new_ys2, new_zs2);
+
+  //Confirm all particles were pushed
+  double EPSILON = 0.0001;
+  for (int i = 0; i < np; ++i) {
+    assert(abs(new_xs1[i] - new_xs2[i]) < EPSILON);
+    assert(abs(new_ys1[i] - new_ys2[i]) < EPSILON);
+    assert(abs(new_zs1[i] - new_zs2[i]) < EPSILON);
+  }
+  
+  //Cleanup
+  delete [] new_xs2;
+  delete [] new_ys2;
+  delete [] new_zs2;
+  delete [] new_xs1;
+  delete [] new_ys1;
+  delete [] new_zs1;
+  delete [] xs;
+  delete [] ys;
+  delete [] zs;
+  delete scs;
+  delete [] ids;
+  delete [] ptcls_per_elem;
+  Kokkos::finalize();
+  return 0;
+}

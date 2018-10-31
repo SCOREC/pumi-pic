998df389077decef052f5db419b7fb55aeedfb6d
Cameron Smith <smithc11@rpi.edu>
2018-10-31 09:41:53 -0400

    only run largest case

--- particle_structs/run_pskk_loops.sh
+++ particle_structs/run_pskk_loops.sh
@@ -5,8 +5,9 @@ C=32
 # blockade can support up to ~150M particles before memory is exhausted on the GPU
 limit=$((150*1000*1000))
 for dist in {1..3}; do
-  for V in 4 8 16 32; do
-    for e in 11 14 17; do
+  for V in 8 16 32 64; do
+    e=17;
+    #for e in 11 14 17; do
       p=10
       log="d${dist}_e${e}_p${p}_C${C}_V${V}_sorted.log"
       cat /dev/null > $log
@@ -19,6 +20,6 @@ for dist in {1..3}; do
       else
         echo "skipping.. limit exceeded!"
       fi
-    done
+    #done
   done
 done

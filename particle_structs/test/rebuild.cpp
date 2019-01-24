f81b3bfb9b42942f68579bfe30b217aca09cbcea
Gerrett Diamond <diamog@rpi.edu>
2019-01-24 14:34:23 -0500

    test rebuild with no changes

--- particle_structs/test/rebuild.cpp
+++ particle_structs/test/rebuild.cpp
@@ -24,16 +24,36 @@ int main(int argc, char* argv[]) {
   SellCSigma<Type, Kokkos::OpenMP>* scs2 = 
     new SellCSigma<Type, Kokkos::OpenMP>(po, 1, 10000, ne, np, ptcls_per_elem, ids, 0);
   
+
   int* new_element = new int[scs->offsets[scs->num_slices]];
+  int* values = scs->getSCS<0>();
+  int* values2 = scs2->getSCS<0>();
   for (int i =0; i < scs->num_slices; ++i) {
     for (int j = scs->offsets[i]; j < scs->offsets[i+1]; j+= ts) {
       for (int k = 0; k < ts; ++k) {
-	new_element[j+k] = j;
+	new_element[j + k] = j;
+	values[j + k] = j + k;
+	values2[j + k] = j + k;
+      }
+    }
+  }
+
+  //Rebuild with no changes  
+  scs->rebuildSCS(new_element);
+
+  values = scs->getSCS<0>();
+  for (int i =0; i < scs->num_slices; ++i) {
+    for (int j = scs->offsets[i]; j < scs->offsets[i+1]; j+= ts) {
+      for (int k = 0; k < ts; ++k) {
+	if (values[j + k] == values2[j + k]) {
+	  printf("Value mismatch at particle %d\n", j+k);
+	}
       }
     }
   }
   
-  scs->rebuildSCS(new_element);
+  delete [] new_element;
 
+  printf("All tests passed\n");
   return 0;
 }

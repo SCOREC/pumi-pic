2fd05a29949020d70c6ae9d5b5528e75c4a45b13
Gopan Perumpilly <gopan.p@gmail.com>
2019-05-05 10:45:38 -0400

    Disabled push_and_search test

--- envRhel7Serial.sh
+++ envRhel7Serial.sh
@@ -1,6 +1,6 @@
 module purge
-module load gcc/7.3.0-bt47fwr \
-  mpich/3.2.1-niuhmad \
-  cmake/3.13.1-ovasnmm \
-  trilinos/develop-debug-openmp-ackkufk \
-  omega-h/9.19.1-sj27mkl
+module load gcc/7.3.0-bt47fwr
+module load mpich/3.3-diz4f6i
+module load cmake/3.14.1-hd7d4ku \
+  trilinos/develop-debug-openmp-me4zjkw \
+  omega-h/9.27.0-mpi-trilinosDebug-cqcs3zy

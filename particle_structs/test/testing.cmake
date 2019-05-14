2fe6027b41f1078a70c5d17e94c2988db9e4097f
Gerrett Diamond <diamog@rpi.edu>
2019-05-14 07:57:13 -0400

    Add some tests to ctest

--- particle_structs/test/testing.cmake
+++ particle_structs/test/testing.cmake
@@ -2,19 +2,8 @@ add_test(NAME type_test COMMAND ./typeTest)
 
 add_test(NAME rebuild COMMAND ./rebuild)
 
-function(test_suite TESTNAME NE NP DIST)
-  add_test(NAME ${TESTNAME} COMMAND ./pskk ${NE} ${NP} ${DIST} 1 ${NP} 0)
-  add_test(NAME ${TESTNAME}_sort COMMAND ./pskk ${NE} ${NP} ${DIST} ${NE} ${NP} 0)
-  add_test(NAME ${TESTNAME}_slice COMMAND ./pskk ${NE} ${NP} ${DIST} 1 32 0)
-  add_test(NAME ${TESTNAME}_sort_slice COMMAND ./pskk ${NE} ${NP} ${DIST} ${NE} 32 0)
-endfunction(test_suite)
+add_test(NAME lambdaTest COMMAND ./lambdaTest)
 
-function(push_test TESTNAME NE NP)
-  test_suite(${TESTNAME}_even ${NE} ${NP} 0)
-  test_suite(${TESTNAME}_uniform ${NE} ${NP} 1)
-  test_suite(${TESTNAME}_gaussian ${NE} ${NP} 2)
-  test_suite(${TESTNAME}_exponential ${NE} ${NP} 3)
-endfunction(push_test)
+add_test(NAME migrateNothing COMMAND ./migrateTest)
 
-#push_test(push_small 100 10000)
-#push_test(push_medium 10000 10000000)
+add_test(NAME migrate4 COMMAND mpirun -np 4 ./migrateTest)

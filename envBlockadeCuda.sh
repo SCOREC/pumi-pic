75fd8202bf779e8366ca639e328ef059aacb6194
Gerrett Diamond <diamog@rpi.edu>
2019-06-18 14:15:17 -0400

    Add environment file for Cuda

---
+++ envBlockadeCuda.sh
@@ -0,0 +1,11 @@
+module purge
+module load gcc
+module load mpich
+module load cmake
+kk=/lore/cwsmith/develop/build-kokkos-blockade-cuda/install
+omega_h=/lore/cwsmith/develop/build-omegah-rhel7-cuda-latest/install
+export CMAKE_PREFIX_PATH=$kk:$omega_h:$CMAKE_PREFIX_PATH
+export MPICH_CXX=/lore/cwsmith/develop/kokkos/bin/nvcc_wrapper
+cuda=/usr/local/cuda-10.1
+export PATH=$cuda/bin:$PATH
+export LD_LIBRARY_PATH=$cuda/lib64:$LD_LIBRARY_PATH

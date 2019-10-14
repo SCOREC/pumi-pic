96a2dc668f19db998cf383f42776b950af482415
Gopan Perumpilly <gopan.p@gmail.com>
2019-10-14 10:59:58 -0400

    renamed Netcdf cmake variables

--- cmake/FindNetCDF.cmake
+++ cmake/FindNetCDF.cmake
@@ -1,38 +1,45 @@
 # - Try to find kokkos
 # Once done this will define
-#  NETCDF_FOUND - System has NETCDF
-#  NETCDF_INCLUDE_DIRS - The NETCDF include directories
-#  NETCDF_LIBRARIES - The libraries needed to use NETCDF
+# NetCDF_FOUND - System has NetCDF
+# NetCDF_INCLUDE_DIRS - The NetCDF include directories
+# NetCDF_LIBRARIES - The libraries needed to use NetCDF
+# NetCDF_VERSION: The version of NetCDF found.
+# NetCDF::NetCDF: A target to use with `target_link_libraries`.
 
-set(NETCDF_PREFIX "" CACHE STRING "NETCDF install directory")
-if(NETCDF_PREFIX)
-  message(STATUS "NETCDF_PREFIX ${NETCDF_PREFIX}")
+set(NetCDF_PREFIX "" CACHE STRING "NetCDF install directory")
+if(NetCDF_PREFIX)
+  message(STATUS "NetCDF_PREFIX ${NetCDF_PREFIX}")
 endif()
 
-find_path(NETCDF_INCLUDE_DIR netcdf PATHS "${NETCDF_PREFIX}/include")
+find_path(NetCDF_INCLUDE_DIR netcdf PATHS "${NetCDF_PREFIX}/include")
 
-find_library(NETCDF_LIBRARY netcdf-cxx4 PATHS "${NETCDF_PREFIX}/lib")
+find_library(NetCDF_LIBRARY netcdf-cxx4 PATHS "${NetCDF_PREFIX}/lib")
 
-set(NETCDF_LIBRARIES ${NETCDF_LIBRARY} )
-set(NETCDF_INCLUDE_DIRS ${NETCDF_INCLUDE_DIR} )
+if (NetCDF_FOUND)
+  #message(STATUS "Found NetCDF : ${NetCDF_FOUND}")
+  message(STATUS "NetCDF version: ${netCDF_VERSION}")
+  set(NetCDF_VERSION ${netCDF_VERSION})
+  set(NetCDF_LIBRARIES ${NetCDF_LIBRARY} )
+  set(NetCDF_INCLUDE_DIRS ${NetCDF_INCLUDE_DIR} )
+endif()
 
 include(FindPackageHandleStandardArgs)
-# handle the QUIETLY and REQUIRED arguments and set NETCDF_FOUND to TRUE
+# handle the QUIETLY and REQUIRED arguments and set NetCDF_FOUND to TRUE
 # if all listed variables are TRUE
 find_package_handle_standard_args(
-    NETCDF
+    NetCDF
     DEFAULT_MSG
-    NETCDF_LIBRARY NETCDF_INCLUDE_DIR
+    NetCDF_LIBRARY NetCDF_INCLUDE_DIR
 )
 
-mark_as_advanced(NETCDF_INCLUDE_DIR NETCDF_LIBRARY )
+mark_as_advanced(NetCDF_INCLUDE_DIR NetCDF_LIBRARY )
 
-if (NETCDF_FOUND)
+if (NetCDF_FOUND)
   if (NOT TARGET NetCDF::NetCDF)
     add_library(NetCDF::NetCDF UNKNOWN IMPORTED)
     set_target_properties(NetCDF::NetCDF PROPERTIES
-      IMPORTED_LOCATION "${NETCDF_LIBRARY}"
-      INTERFACE_INCLUDE_DIRECTORIES "${NETCDF_INCLUDE_DIR}")
+      IMPORTED_LOCATION "${NetCDF_LIBRARY}"
+      INTERFACE_INCLUDE_DIRECTORIES "${NetCDF_INCLUDE_DIR}")
   endif ()
 endif ()
 

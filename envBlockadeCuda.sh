5eec091a9f17cf43f46de63b4bf2dbbc77aaa1a3
Gopan Perumpilly <gopan.p@gmail.com>
2019-10-30 14:39:52 -0400

    added netcdf in envBlockade file

--- envBlockadeCuda.sh
+++ envBlockadeCuda.sh
@@ -2,10 +2,17 @@ module purge
 module load gcc
 module load mpich
 module load cmake
+module load netcdf
+module load vim
+ncxx=/lore/gopan/install/build-netcdfcxx431/install
+export NETCDF_PREFIX=$ncxx
+
 kk=/lore/cwsmith/develop/build-kokkos-blockade-cuda/install
 omega_h=/lore/cwsmith/develop/build-omegah-blockade-cuda/install
-export CMAKE_PREFIX_PATH=$kk:$omega_h:$CMAKE_PREFIX_PATH
+export CMAKE_PREFIX_PATH=$kk:$omega_h:$ncxx:$CMAKE_PREFIX_PATH
 export MPICH_CXX=/lore/cwsmith/develop/kokkos/bin/nvcc_wrapper
 cuda=/usr/local/cuda-10.1
 export PATH=$cuda/bin:$PATH
 export LD_LIBRARY_PATH=$cuda/lib64:$LD_LIBRARY_PATH
+
+# /lore/gopan/install/build-blockade-ps-noOpt/install

82a2c518e6a252f4960325ba5dca8ec09d4b54de
Gopan Perumpilly <gopan.p@gmail.com>
2019-12-22 20:56:36 -0500

    updated env with latest install

--- envBlockadeCuda.sh
+++ envBlockadeCuda.sh
@@ -2,15 +2,15 @@ module load gcc
 module load mpich
 module load cmake
 module load netcdf
-module load vim
 ncxx=/lore/gopan/install/build-netcdfcxx431/install
-export NETCDF_PREFIX=$ncxx
+#export NetCDF_PREFIX=$ncxx
+export MY_NCXX=$ncxx #/lib64/cmake/netCDFCxx
 kk=/lore/gopan/install/build-kokkos-cuda-debug-profile-rhel7/install
 omega_h=/lore/gopan/install/build-omegah-cuda-rhel7/install
-ps=/lore/gopan/install/particle_structures_deb_prof/install
+ps=/lore/gopan/install/build_particle_structures_deb_prof/install
 cuda=/usr/local/cuda-10.1
 export PATH=$cuda/bin:$PATH
-export LD_LIBRARY_PATH=$cuda/lib64:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$cuda/lib64:$ncxx/lib64:$LD_LIBRARY_PATH
 export CMAKE_PREFIX_PATH=${kk}/lib/CMake/Kokkos/:$omega_h:$ps:$ncxx:$CMAKE_PREFIX_PATH
 kksrc=/lore/gopan/install/kokkos
 export MPICH_CXX=${kksrc}/bin/nvcc_wrapper

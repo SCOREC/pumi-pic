8b380258c6d42d335c53ce9541eb79d8be47e12c
Gerrett Diamond <diamog@rpi.edu>
2020-01-05 14:29:03 -0500

    Fixes in order to use new particle_structures setup

--- src/pumipic_kktypes.hpp
+++ src/pumipic_kktypes.hpp
@@ -13,16 +13,17 @@ namespace pumipic {
   using particle_structs::Vector3d;
 
   typedef Kokkos::DefaultExecutionSpace exe_space;
-  typedef particle_structs::Segment<int, exe_space> SegmentInt;
-  typedef particle_structs::Segment<Vector3d, exe_space> Segment3d;
+  typedef exe_space::device_type device_type;
+  typedef particle_structs::Segment<int, device_type> SegmentInt;
+  typedef particle_structs::Segment<Vector3d, device_type> Segment3d;
 
-  typedef Kokkos::View<lid_t*, exe_space::device_type> kkLidView;
+  typedef Kokkos::View<lid_t*, device_type> kkLidView;
   void hostToDeviceLid(kkLidView d, lid_t *h);
   void deviceToHostLid(kkLidView d, lid_t *h);
-  typedef Kokkos::View<fp_t*, exe_space::device_type> kkFpView;
+  typedef Kokkos::View<fp_t*, device_type> kkFpView;
   /** \brief helper function to transfer a host array to a device view */
   void hostToDeviceFp(kkFpView d, fp_t* h);
-  typedef Kokkos::View<Vector3d*, exe_space::device_type> kkFp3View;
+  typedef Kokkos::View<Vector3d*, device_type> kkFp3View;
   /** \brief helper function to transfer a host array to a device view */
   void hostToDeviceFp(kkFp3View d, fp_t (*h)[3]);
   void deviceToHostFp(kkFp3View d, fp_t (*h)[3]);

133951843e50847d5d20acbf35ebf76c79da2909
Gerrett Diamond <diamog@rpi.edu>
2020-01-28 13:37:03 -0500

    Add more tests for Device-Host copying

--- particle_structs/test/test_structure.cpp
+++ particle_structs/test/test_structure.cpp
@@ -18,6 +18,7 @@ int addCSRs(std::vector<PS*>& structures, std::vector<std::string>& names,
 //Simple tests of construction
 int testCounts(const char* name, PS* structure, lid_t num_elems, lid_t num_ptcls);
 int testParticleExistence(const char* name, PS* structure, lid_t num_ptcls);
+int setValues(const char* name, PS* structure);
 
 //Functionality tests
 int testRebuild(const char* name, PS* structure);
@@ -74,6 +75,7 @@ int main(int argc, char* argv[]) {
     for (int i = 0; i < structures.size(); ++i) {
       fails += testCounts(names[i].c_str(), structures[i], num_elems, num_ptcls);
       fails += testParticleExistence(names[i].c_str(), structures[i], num_ptcls);
+      fails += setValues(names[i].c_str(), structures[i]);
       fails += testMetrics(names[i].c_str(), structures[i]);
       fails += testCopy(names[i].c_str(), structures[i]);
     }
@@ -202,6 +204,32 @@ int testParticleExistence(const char* name, PS* structure, lid_t num_ptcls) {
   return fails;
 }
 
+int setValues(const char* name, PS* structure) {
+  int fails = 0;
+  auto dbls = structure->get<1>();
+  auto nums = structure->get<2>();
+  auto bools = structure->get<3>();
+  int local_rank = comm_rank;
+  auto setValues = PS_LAMBDA(const lid_t& e, const lid_t& p, const bool& mask) {
+    if (mask) {
+      dbls(p, 0) = p * e * 100.0;
+      dbls(p, 1) = M_PI * p + M_PI / 2.0;
+      dbls(p, 2) = M_E * 2.5;
+      nums(p) = local_rank;
+      bools(p) = true;
+    }
+    else {
+      dbls(p, 0) = 0;
+      dbls(p, 1) = 0;
+      dbls(p, 2) = 0;
+      nums(p) = -1;
+      bools(p) = false;
+    }
+  };
+  ps::parallel_for(structure, setValues, "setValues");
+  return fails;
+}
+
 //Functionality tests
 int testRebuild(const char* name, PS* structure) {
   int fails = 0;
@@ -249,8 +277,65 @@ int testCopy(const char* name, PS* structure) {
     ++fails;
   }
   //Copy particle structure back to the device
-
+  PS* device_structure = ps::copy<Kokkos::CudaSpace>(host_structure);
+  if (device_structure->nElems() != structure->nElems()) {
+    fprintf(stderr, "[ERROR] Test %s: Failed to copy nElems() back to device on rank %d\n",
+            name, comm_rank);
+    ++fails;
+  }
+  if (device_structure->nPtcls() != structure->nPtcls()) {
+    fprintf(stderr, "[ERROR] Test %s: Failed to copy nPtcls() back to device on rank %d\n",
+            name, comm_rank);
+    ++fails;
+  }
+  if (device_structure->capacity() != structure->capacity()) {
+    fprintf(stderr, "[ERROR] Test %s: Failed to copy capacity() back to device on rank %d\n",
+            name, comm_rank);
+    ++fails;
+  }
+  if (device_structure->numRows() != structure->numRows()) {
+    fprintf(stderr, "[ERROR] Test %s: Failed to copy numRows() back to device on rank %d\n",
+            name, comm_rank);
+    ++fails;
+  }
   //Compare original and new particle structure on the device
+  auto ids1 = structure->get<0>();
+  auto ids2 = device_structure->get<0>();
+  auto dbls1 = structure->get<1>();
+  auto dbls2 = device_structure->get<1>();
+  double EPSILON = .00001;
+  kkLidView failure("failure", 1);
+  int local_rank = comm_rank;
+  auto testTypes = PS_LAMBDA(const int& eid, const int& pid, const bool mask) {
+    if (mask) {
+      if (ids1(pid) != ids2(pid)) {
+        printf("[ERROR] Test %s: Particle ids do not match for particle %d "
+               "[(old) %d != %d (copy)] on rank %d\n", name, pid, ids1(pid),
+               ids2(pid), local_rank);
+        failure(0) = 1;
+      }
+      for (int i = 0; i < 3; ++i)
+        if (fabs(dbls1(pid,i) - dbls2(pid,i)) > EPSILON) {
+          printf("[ERROR] Test %s: Particle's dbl %d does not match for particle %d"
+                 "[(old) %.4f != %.4f (copy)] on rank %d\n", name, i, pid, dbls1(pid,i),
+                 dbls2(pid,i), local_rank);
+          failure(0) = 1;
+      }
+    }
+  };
+  ps::parallel_for(structure, testTypes, "testTypes on original structure");
+  if (ps::getLastValue<lid_t>(failure)) {
+    fprintf(stderr, "[ERROR] Test %s: Parallel for on original structure had failures\n",
+            name);
+    ++fails;
+  }
 
+  Kokkos::parallel_for(1, KOKKOS_LAMBDA(const int& i) {failure(i) = 0;});
+  ps::parallel_for(device_structure, testTypes, "testTypes on copy of structure");
+  if (ps::getLastValue<lid_t>(failure)) {
+    fprintf(stderr, "[ERROR] Test %s: Parallel for on device structure had failures\n",
+            name);
+    ++fails;
+  }
   return fails;
 }

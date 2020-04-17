e04ea04809a1cb09f08d8895f064bc268f451004
Gerrett Diamond <diamog@rpi.edu>
2020-04-17 17:23:42 -0400

    Add Isend/Irecv/Wait test

--- support/ViewComm_test.cpp
+++ support/ViewComm_test.cpp
@@ -12,6 +12,20 @@ int allToAllTest(const char* name, int msg_size);
 template <typename Space>
 int reductionTest(const char* name);
 
+template <typename Space>
+int runTests() {
+  int fails = 0;
+
+  fails += sendRecvTest<Space>("Small send/recv",10);
+  fails += sendRecvTest<Space>("Large send/recv",10000);
+
+  fails += iSendRecvWaitTest<Space>("Small Isend/Irecv + Wait", 10);
+  fails += iSendRecvWaitTest<Space>("Large Isend/Irecv + Wait", 10000);
+
+  fails += iSendRecvWaitAllTest<Space>("Isend/Irecv + Waitall");
+
+  return fails;
+}
 int main(int argc, char* argv[]) {
   Kokkos::initialize(argc, argv);
   MPI_Init(&argc, &argv);
@@ -22,18 +36,16 @@ int main(int argc, char* argv[]) {
   int fails = 0;
 
   //Test Host Functions
-  fails += sendRecvTest<Kokkos::HostSpace>("Small send/recv",10);
-  fails += sendRecvTest<Kokkos::HostSpace>("Large send/recv",10000);
-  fails += iSendRecvWaitAllTest<Kokkos::HostSpace>("Isend/Irecv + Waitall");
+  fails += runTests<Kokkos::HostSpace>();
 
   //Test Cuda Functions
-  fails += sendRecvTest<Kokkos::CudaSpace>("Small send/recv",10);
-  fails += sendRecvTest<Kokkos::CudaSpace>("Large send/recv",10000);
-  fails += iSendRecvWaitAllTest<Kokkos::CudaSpace>("Isend/Irecv + Waitall");
+  fails += runTests<Kokkos::CudaSpace>();
+
   MPI_Finalize();
   Kokkos::finalize();
   return fails;
 }
+
 template <typename Space>
 int sendRecvTest(const char* name, int msg_size) {
   //Setup
@@ -111,12 +123,173 @@ int sendRecvTest(const char* name, int msg_size) {
   }
 
   MPI_Barrier(MPI_COMM_WORLD);
-
   //Closing
   fails += pumipic::getLastValue<int>(device_fails);
   int final_fail;
   MPI_Allreduce(&fails, &final_fail, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
   return final_fail > 0;
+}
+
+template <typename Space>
+int iSendRecvWaitTest(const char* name, int msg_size) {
+  //Setup
+  if (!comm_rank)
+    printf("Beginning Test %s_%s\n", name, Space::name());
+  int fails = 0;
+  Kokkos::View<int*, Space> device_fails("failures", 1);
+  int local_rank = comm_rank;
+  int local_size = comm_size;
+  typedef Kokkos::RangePolicy<typename Space::execution_space> ExecPolicy;
+  typename Space::execution_space exec;
+
+  //Nonblocking Send and recv msg_size elements to the two neighboring ranks and Wait.
+  //Kokkos View Test
+  if (!comm_rank)
+    printf("  Kokkos View Test\n");
+  {
+    int prev_rank = (comm_rank - 1 + local_size) % local_size;
+    int next_rank = (comm_rank + 1) % local_size;
+    MPI_Request send_requests[2];
+    MPI_Request recv_requests[2];
+    Kokkos::View<int*, Space> send_view("send view", msg_size * 2);
+    Kokkos::View<int*, Space> recv_view("recv view", msg_size * 2);
+    Kokkos::parallel_for(ExecPolicy(exec, 0, msg_size), KOKKOS_LAMBDA(const int i) {
+      send_view(i) = local_rank;
+      send_view(i + msg_size) = local_rank;
+    });
+    int ret = pumipic::PS_Comm_Isend(send_view, 0, msg_size, prev_rank, 0, MPI_COMM_WORLD,
+                                     send_requests);
+    if (ret != MPI_SUCCESS) {
+      fprintf(stderr, "[ERROR] Rank %d: PS_Comm_Isend to %d returned error code %d\n",
+              comm_rank, prev_rank, ret);
+      ++fails;
+    }
+    ret = pumipic::PS_Comm_Isend(send_view, msg_size, msg_size, next_rank, 1, MPI_COMM_WORLD,
+                                 send_requests + 1);
+    if (ret != MPI_SUCCESS) {
+      fprintf(stderr, "[ERROR] Rank %d: PS_Comm_Isend to %d returned error code %d\n",
+              comm_rank, next_rank, ret);
+      ++fails;
+    }
+    ret = pumipic::PS_Comm_Irecv(recv_view, 0, msg_size, prev_rank, 1, MPI_COMM_WORLD,
+                                 recv_requests);
+    if (ret != MPI_SUCCESS) {
+      fprintf(stderr, "[ERROR] Rank %d: PS_Comm_Irecv to %d returned error code %d\n",
+              comm_rank, prev_rank, ret);
+      ++fails;
+    }
+    ret = pumipic::PS_Comm_Irecv(recv_view, msg_size, msg_size, next_rank, 0, MPI_COMM_WORLD,
+                                 recv_requests + 1);
+    if (ret != MPI_SUCCESS) {
+      fprintf(stderr, "[ERROR] Rank %d: PS_Comm_Irecv to %d returned error code %d\n",
+              comm_rank, next_rank, ret);
+      ++fails;
+    }
+    MPI_Status status;
+    for (int i = 0; i < 2; ++i) {
+      ret = pumipic::PS_Comm_Wait<Space>(send_requests + i, &status);
+      if (ret != MPI_SUCCESS) {
+        fprintf(stderr, "[ERROR] Rank %d: PS_Comm_Wait on send request %d "
+                "returned error code %d\n", comm_rank, i, ret);
+      }
+      ret = pumipic::PS_Comm_Wait<Space>(recv_requests + i, &status);
+      if (ret != MPI_SUCCESS) {
+        fprintf(stderr, "[ERROR] Rank %d: PS_Comm_Wait on recv request %d "
+                "returned error code %d\n", comm_rank, i, ret);
+      }
+    }
+    Kokkos::parallel_for(ExecPolicy(exec, 0, msg_size), KOKKOS_LAMBDA(const int i) {
+        if (recv_view(i) != prev_rank) {
+          printf("[ERROR] Rank %d: Recevied incorrect value in element %d "
+                 "[(actual) %d != %d (should be)]\n", local_rank, i, recv_view(i), prev_rank);
+          Kokkos::atomic_add(&(device_fails(0)), 1);
+        }
+        if (recv_view(i + msg_size) != next_rank) {
+          printf("[ERROR] Rank %d: Recevied incorrect value in element %d "
+                 "[(actual) %d != %d (should be)]\n", local_rank, i + msg_size,
+                 recv_view(i + msg_size), next_rank);
+          Kokkos::atomic_add(&(device_fails(0)), 1);
+        }
+    });
+  }
+
+  MPI_Barrier(MPI_COMM_WORLD);
+  if (!comm_rank)
+    printf("  PUMIPic View Test\n");
+  {
+    int prev_rank = (comm_rank - 1 + local_size) % local_size;
+    int next_rank = (comm_rank + 1) % local_size;
+    MPI_Request send_requests[2];
+    MPI_Request recv_requests[2];
+    pumipic::View<int*, Space> send_view("send view", msg_size * 2);
+    pumipic::View<int*, Space> recv_view("recv view", msg_size * 2);
+    Kokkos::parallel_for(ExecPolicy(exec, 0, msg_size), KOKKOS_LAMBDA(const int i) {
+      send_view(i) = local_rank;
+      send_view(i + msg_size) = local_rank;
+    });
+    int ret = pumipic::PS_Comm_Isend(send_view, 0, msg_size, prev_rank, 0, MPI_COMM_WORLD,
+                                     send_requests);
+    if (ret != MPI_SUCCESS) {
+      fprintf(stderr, "[ERROR] Rank %d: PS_Comm_Isend to %d returned error code %d\n",
+              comm_rank, prev_rank, ret);
+      ++fails;
+    }
+    ret = pumipic::PS_Comm_Isend(send_view, msg_size, msg_size, next_rank, 1, MPI_COMM_WORLD,
+                                 send_requests + 1);
+    if (ret != MPI_SUCCESS) {
+      fprintf(stderr, "[ERROR] Rank %d: PS_Comm_Isend to %d returned error code %d\n",
+              comm_rank, next_rank, ret);
+      ++fails;
+    }
+    ret = pumipic::PS_Comm_Irecv(recv_view, 0, msg_size, prev_rank, 1, MPI_COMM_WORLD,
+                                 recv_requests);
+    if (ret != MPI_SUCCESS) {
+      fprintf(stderr, "[ERROR] Rank %d: PS_Comm_Irecv to %d returned error code %d\n",
+              comm_rank, prev_rank, ret);
+      ++fails;
+    }
+    ret = pumipic::PS_Comm_Irecv(recv_view, msg_size, msg_size, next_rank, 0, MPI_COMM_WORLD,
+                                 recv_requests + 1);
+    if (ret != MPI_SUCCESS) {
+      fprintf(stderr, "[ERROR] Rank %d: PS_Comm_Irecv to %d returned error code %d\n",
+              comm_rank, next_rank, ret);
+      ++fails;
+    }
+    MPI_Status status;
+    for (int i = 0; i < 2; ++i) {
+      ret = pumipic::PS_Comm_Wait<Space>(send_requests + i, &status);
+      if (ret != MPI_SUCCESS) {
+        fprintf(stderr, "[ERROR] Rank %d: PS_Comm_Wait on send request %d "
+                "returned error code %d\n", comm_rank, i, ret);
+      }
+      ret = pumipic::PS_Comm_Wait<Space>(recv_requests + i, &status);
+      if (ret != MPI_SUCCESS) {
+        fprintf(stderr, "[ERROR] Rank %d: PS_Comm_Wait on recv request %d "
+                "returned error code %d\n", comm_rank, i, ret);
+      }
+    }
+    Kokkos::parallel_for(ExecPolicy(exec, 0, msg_size), KOKKOS_LAMBDA(const int i) {
+        if (recv_view(i) != prev_rank) {
+          printf("[ERROR] Rank %d: Recevied incorrect value in element %d "
+                 "[(actual) %d != %d (should be)]\n", local_rank, i, recv_view(i), prev_rank);
+          Kokkos::atomic_add(&(device_fails(0)), 1);
+        }
+        if (recv_view(i + msg_size) != next_rank) {
+          printf("[ERROR] Rank %d: Recevied incorrect value in element %d "
+                 "[(actual) %d != %d (should be)]\n", local_rank, i + msg_size,
+                 recv_view(i + msg_size), next_rank);
+          Kokkos::atomic_add(&(device_fails(0)), 1);
+        }
+    });
+  }
+
+  MPI_Barrier(MPI_COMM_WORLD);
+  //Closing
+  fails += pumipic::getLastValue<int>(device_fails);
+  int final_fail;
+  MPI_Allreduce(&fails, &final_fail, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
+  return final_fail > 0;
+
 }
 
 template <typename Space>
@@ -233,6 +406,7 @@ int iSendRecvWaitAllTest(const char* name) {
     });
   }
 
+  MPI_Barrier(MPI_COMM_WORLD);
   //Closing
   fails += pumipic::getLastValue<int>(device_fails);
   int final_fail;

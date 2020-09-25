b342fda9b9151223a4df52aaed4bde29b8ae44c6
Steven Spreizer <spreis@rpi.edu>
2020-09-24 21:06:30 -0400

    Destroyed and added particles test completed

--- particle_structs/test/rebuild_csr.cpp
+++ particle_structs/test/rebuild_csr.cpp
@@ -56,10 +56,10 @@ int main(int argc, char* argv[]){
     ///fails += rebuildNewPtcls();
 
     //check for particles that were removed and the rest in their correct loc
-    fails += rebuildPtclsDestroyed();
+    //fails += rebuildPtclsDestroyed();
 
     //complete check of rebuild functionality
-    //fails += rebuildNewAndDestroyed();
+    fails += rebuildNewAndDestroyed();
 
   }
   //end Kokkos scope
@@ -507,6 +507,116 @@ int rebuildPtclsDestroyed(){
 int rebuildNewAndDestroyed(){
   Kokkos::Profiling::pushRegion("rebuildNewAndDestroyed");
   int fails = 0;
+
+  //Test construction based on SCS testing
+  int ne = 5;
+  int np = 20;
+  int* ptcls_per_elem = new int[ne];
+  std::vector<int>* ids = new std::vector<int>[ne];
+  distribute_particles(ne, np, 0, ptcls_per_elem, ids); 
+  Kokkos::TeamPolicy<exe_space> po(32,Kokkos::AUTO);
+  CSR::kkLidView ptcls_per_elem_v("ptcls_per_elem_v",ne);
+  CSR::kkGidView element_gids_v("",0);
+  particle_structs::hostToDevice(ptcls_per_elem_v,ptcls_per_elem);
+
+  CSR* csr = new CSR(po, ne, np, ptcls_per_elem_v, element_gids_v);
+  CSR* csr2 = new CSR(po, ne, np, ptcls_per_elem_v, element_gids_v);
+
+  delete [] ptcls_per_elem;
+  delete [] ids;
+//  fprintf(stderr,"csr inited\n");
+//  printView(ps::getMemberView<Type,0>(csr->getPtcl_data()));
+//  fprintf(stderr,"csr2 inited\n");
+//  printView(ps::getMemberView<Type,0>(csr2->getPtcl_data()));
+
+  CSR::kkLidView new_element("new_element", csr->getCapacity());
+  
+  auto values = csr->get<0>();
+  auto values2 = csr2->get<0>();
+  auto offsets_cpy = csr->getOffsets();
+
+  //"Send To Self"
+  Kokkos::parallel_for("sendToSelf", np, 
+      KOKKOS_LAMBDA (const int& i) {
+    //assigns new_elements to be the current elements and fills values and
+    //values2 for comparison after the rebuild
+
+    lid_t elem = 0;
+    while(offsets_cpy(elem+1) < i)
+      elem++;
+
+    new_element(i) = elem; //change to assign to diff elems
+    if(i%7 == 0) new_element(i) = -1;
+    values(i) = i;
+    values2(i) = i;  
+
+  });
+
+  //////////////////////////////////////////////////////////////
+  //Introduce new particles
+  int nnp = 5;
+  CSR::kkLidView new_particle_elements("new_particle_elements", nnp);
+  auto new_particles = particle_structs::createMemberViews<Type>(nnp);
+  auto new_particle_access = particle_structs::getMemberView<Type,0>(new_particles);
+   
+  Kokkos::parallel_for("new_particle_elements", nnp, 
+      KOKKOS_LAMBDA (const int& i){
+      new_particle_elements(i) = i%5;
+      new_particle_access(i) = i%5+np;
+  });
+
+
+
+
+
+  //fprintf(stderr,"csr data check:\n");
+  //printView(ps::getMemberView<Type,0>(csr->getPtcl_data()));
+
+  //fprintf(stderr,"csr2 data check:\n");
+  //printView(ps::getMemberView<Type,0>(csr2->getPtcl_data()));
+
+  //fprintf(stderr,"Rebuild next\n");
+  //Rebuild with elements removed 
+  csr->rebuild(new_element,new_particle_elements,new_particles);
+  values = csr->get<0>();
+  offsets_cpy = csr->getOffsets();
+
+  auto pIDs  = ps::getMemberView<Type,0>(csr->getPtcl_data());
+  auto pIDs2  = ps::getMemberView<Type,0>(csr2->getPtcl_data());
+
+  printView(pIDs);
+  printView(pIDs2);
+
+  lid_t particles = csr->getCapacity();
+  printf("particles after removal: %d\n",particles);
+  kkLidView failed = kkLidView("failed", 1);
+  Kokkos::parallel_for("check no changes", np,
+      KOKKOS_LAMBDA (const int& i){
+    lid_t id = values2(i);
+    lid_t dest_elem = new_element(i);
+    bool found = false;
+    if(dest_elem == -1){
+      for(lid_t i = 0; i < particles; ++i){
+        assert(values(i) != id);
+      }
+      found = true;
+    }
+    else{
+      lid_t row_start = offsets_cpy(dest_elem);
+      lid_t row_end =offsets_cpy(dest_elem+1);
+      for(lid_t i = row_start; i < row_end; ++i){
+        if(values(i) == id){
+          found = true;
+          break;
+        } 
+      }
+    }
+    if(!found) failed(0)+=1;
+  });
+  
+  fails += getLastValue<lid_t>(failed);
+  fprintf(stderr,"end\n");
+
 
 
   Kokkos::Profiling::popRegion();

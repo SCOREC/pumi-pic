18f6d92f8a49fcadbd8c67fd7dae7f446f77e31b
Cameron Smith <smithc11@rpi.edu>
2020-10-01 08:43:46 -0400

    csr tests depend on write_ptcls* tests

--- particle_structs/test/testing.cmake
+++ particle_structs/test/testing.cmake
@@ -6,9 +6,6 @@ add_test(NAME initParticles COMMAND ./initParticles)
 
 add_test(NAME buildSCS COMMAND ./buildSCSTest)
 
-add_test(NAME test_csr_small COMMAND ./test_csr small_ptcls_e5_p25_r0)
-add_test(NAME test_csr_small2 COMMAND ./test_csr small_ptcls_e5_p25_r4)
-add_test(NAME test_csr_small3 COMMAND ./test_csr small_ptcls_e5_p25_r4_edit)
 
 add_test(NAME scs_padding COMMAND ./test_scs_padding)
 
@@ -35,6 +32,10 @@ add_test(NAME write_ptcl_4 COMMAND mpirun -np 4 ./write_particles 100 10000 0 2
   small_ptcls_e100_p10k_r4)
 add_test(NAME write_ptcl_empty COMMAND mpirun -np 4 ./write_particles 0 0 0 0 empty_ptcls)
 add_test(NAME write_ptcl_noptcls COMMAND mpirun -np 4 ./write_particles 100 0 0 0 no_ptcls_e100)
+
+add_test(NAME test_csr_small COMMAND ./test_csr small_ptcls_e5_p25_r0)
+add_test(NAME test_csr_small2 COMMAND ./test_csr small_ptcls_e5_p25_r4)
+add_test(NAME test_csr_small3 COMMAND ./test_csr small_ptcls_e5_p25_r4_edit)
 
 add_test(NAME test_structures_small COMMAND ./test_structure small_ptcls_e5_p25_r0)
 add_test(NAME test_structures_small_4 COMMAND mpirun -np 4

750cd2d41a09aa5533c26ed26a68792dd7f2e4f7
Gerrett Diamond <diamog@rpi.edu>
2020-10-05 15:22:54 -0400

    Add performance test for rebuild

--- particle_structs/test/Distribute.h
+++ particle_structs/test/Distribute.h
@@ -3,12 +3,89 @@
 
 #include <vector>
 #include <SCS_Types.h>
+#include <Kokkos_Random.hpp>
+#include <particle_structs.hpp>
+#include <SupportKK.h>
 
 bool distribute_elements(int ne, int strat, int comm_rank, int comm_size, pumipic::gid_t* gids);
 
 bool distribute_particles(int ne, int np, int strat, int* ptcls_per_elem,
                           std::vector<int>* ids);
 
+template <typename PS>
+bool redistribute_particles(PS* ptcls, int strat, double percentMoved,
+                            typename PS::kkLidView new_elms);
+
 const char* distribute_name(int strat);
+
+
+//Define a seed so each call are roughly the "same" across different runs
+#define DISTRIBUTE_SEED 1024 * 1024
+template <typename PS>
+bool redistribute_particles(PS* ptcls, int strat, double percentMoved,
+                            typename PS::kkLidView new_elems) {
+  Kokkos::Random_XorShift64_Pool<typename PS::execution_space> pool(DISTRIBUTE_SEED);
+  typename PS::kkLidView is_moving("is_moving", ptcls->capacity());
+  auto decideMovers = PS_LAMBDA(const int e, const int p, const bool mask) {
+    if (mask) {
+      auto generator = pool.get_state();
+      double prob = generator.drand(1.0);
+      pool.free_state(generator);
+      is_moving[p] = (prob <= percentMoved);
+    }
+  };
+  pumipic::parallel_for(ptcls, decideMovers, "decideMovers");
+
+  typename PS::kkLidView mover_index("mover_index", ptcls->capacity());
+  Kokkos::parallel_scan("indexMovers", is_moving.size(),
+                        KOKKOS_LAMBDA(const int i, int& update, const bool fin) {
+                          if (fin) {
+                            mover_index[i] = -1;
+                            if (is_moving[i])
+                              mover_index[i] = update;
+                          }
+                          update+= is_moving[i];
+                        });
+  int total = 0;
+  Kokkos::parallel_reduce("countMovers", is_moving.size(),
+                          KOKKOS_LAMBDA(const int i, int& update) {
+                            update += is_moving[i];
+                          }, total);
+
+  int numElems = ptcls->nElems();
+  int* ptcls_per_elm = new int[numElems];
+  std::vector<int>* ids = new std::vector<int>[numElems];
+  distribute_particles(numElems, total, strat, ptcls_per_elm, ids);
+
+  int* new_moves = new int[total];
+  for (int i = 0; i < numElems; ++i) {
+    for (std::size_t j = 0; j < ids[i].size(); ++j) {
+      new_moves[ids[i][j]] = i;
+    }
+  }
+  delete [] ptcls_per_elm;
+  delete [] ids;
+
+  typename PS::kkLidView new_moves_d("new_moves", total);
+  pumipic::hostToDevice(new_moves_d, new_moves);
+
+  auto assignMoves = PS_LAMBDA(const int e, const int p, const bool mask) {
+    if (mask) {
+      if (is_moving[p]) {
+        int index = mover_index[p];
+        new_elems[p] = new_moves_d[index];
+      }
+      else {
+        new_elems[p] = e;
+      }
+    }
+    else {
+      new_elems[p] = -1;
+    }
+  };
+  pumipic::parallel_for(ptcls, assignMoves, "assignMoves");
+  delete [] new_moves;
+  return true;
+}
 
 #endif
---
+++ performance_tests/ps_rebuild.cpp
@@ -0,0 +1,95 @@
+#include <particle_structs.hpp>
+#include <ppTiming.hpp>
+#include "perfTypes.hpp"
+#include "../particle_structs/test/Distribute.h"
+
+PS* createSCS(int num_elems, int num_ptcls, kkLidView ppe, kkGidView elm_gids, int C, int sigma, int V);
+PS* createCSR(int num_elems, int num_ptcls, kkLidView ppe, kkGidView elm_gids);
+
+int main(int argc, char* argv[]) {
+  Kokkos::initialize(argc, argv);
+  MPI_Init(&argc, &argv);
+
+  /* Check commandline arguments */
+  if (argc != 5) {
+    fprintf(stderr, "Usage: %s <num elems> <num ptcls> <distribution> <%% ptcls move>\n",
+            argv[0]);
+  }
+
+  /* Enable timing on every process */
+  pumipic::SetTimingVerbosity(0);
+
+  {
+    /* Create initial distribution of particles */
+    int num_elems = atoi(argv[1]);
+    int num_ptcls = atoi(argv[2]);
+    int strat = atoi(argv[3]);
+    kkLidView ppe("ptcls_per_elem", num_elems);
+    kkGidView element_gids("",0);
+    int* ppe_host = new int[num_elems];
+    std::vector<int>* ids = new std::vector<int>[num_elems];
+    distribute_particles(num_elems, num_ptcls, strat, ppe_host, ids);
+    pumipic::hostToDevice(ppe, ppe_host);
+    delete [] ppe_host;
+    delete [] ids;
+
+
+    /* Create particle structure */
+    ParticleStructures structures;
+    structures.push_back(std::make_pair("Sell-32-ne",
+                                        createSCS(num_elems, num_ptcls, ppe, element_gids,
+                                                  32, num_elems, 1024)));
+    structures.push_back(std::make_pair("Sell-16-ne",
+                                        createSCS(num_elems, num_ptcls, ppe, element_gids,
+                                                  16, num_elems, 1024)));
+    structures.push_back(std::make_pair("Sell-32-1024",
+                                        createSCS(num_elems, num_ptcls, ppe, element_gids,
+                                                  32, 1024, 1024)));
+    structures.push_back(std::make_pair("Sell-16-1024",
+                                        createSCS(num_elems, num_ptcls, ppe, element_gids,
+                                                  16, 1024, 1024)));
+    structures.push_back(std::make_pair("Sell-32-1",
+                                        createSCS(num_elems, num_ptcls, ppe, element_gids,
+                                                  32, 1, 1024)));
+    structures.push_back(std::make_pair("Sell-16-1",
+                                        createSCS(num_elems, num_ptcls, ppe, element_gids,
+                                                  16, 1, 1024)));
+    // structures.push_back(std::make_pair("CSR",
+    //                                     createCSR(num_elems, num_ptcls, ppe, element_gids)));
+
+    const int ITERS = 100;
+    printf("Performing %d iterations of rebuild on each structure\n", ITERS);
+    /* Perform rebuild on particle structures */
+    double percentMoved = atof(argv[4]);
+    for (int i = 0; i < structures.size(); ++i) {
+      std::string name = structures[i].first;
+      PS* ptcls = structures[i].second;
+      printf("Beginning rebuild on structure %s\n", name.c_str());
+      for (int i = 0; i < ITERS; ++i) {
+        kkLidView new_elms("new elems", ptcls->capacity());
+        redistribute_particles(ptcls, strat, percentMoved, new_elms);
+        Kokkos::Timer rebuild_timer;
+        ptcls->rebuild(new_elms);
+        float rebuild_time = rebuild_timer.seconds();
+        pumipic::RecordTime(name.c_str(), rebuild_time);
+      }
+    }
+
+    for (size_t i = 0; i < structures.size(); ++i)
+      delete structures[i].second;
+    structures.clear();
+  }
+
+  pumipic::SummarizeTime();
+  Kokkos::finalize();
+  return 0;
+}
+
+PS* createSCS(int num_elems, int num_ptcls, kkLidView ppe, kkGidView elm_gids, int C, int sigma, int V) {
+  Kokkos::TeamPolicy<ExeSpace> policy(4, C);
+  pumipic::SCS_Input<PerfTypes> input(policy, sigma, V, num_elems, num_ptcls, ppe, elm_gids);
+  return new pumipic::SellCSigma<PerfTypes, MemSpace>(input);
+}
+PS* createCSR(int num_elems, int num_ptcls, kkLidView ppe, kkGidView elm_gids) {
+  return new pumipic::CSR<PerfTypes, MemSpace>(num_elems, num_ptcls, ppe, elm_gids);
+}

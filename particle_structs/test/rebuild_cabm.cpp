3eda486215706cef4429763ea0c2b99f6161b049
MatthewChristoff <matthew.christoff2000@gmail.com>
2021-02-08 14:51:04 -0500

    Updated tests to better reflect new tests on master branch for CSR (which fixed some indexing issues), all current tests pass

--- particle_structs/test/rebuild_cabm.cpp
+++ particle_structs/test/rebuild_cabm.cpp
@@ -55,7 +55,6 @@ int main(int argc, char* argv[]) {
     fprintf(stderr,"RebuildNoChanges\n");
     fails += rebuildNoChanges(number_elements,number_particles,distribution);
 
-    
     //verify all elements are correctly assigned to new elements
     fprintf(stderr,"RebuildNewElems\n");
     fails += rebuildNewElems(number_elements,number_particles,distribution);
@@ -79,35 +78,6 @@ int main(int argc, char* argv[]) {
   return fails;
 }
 
-int rebuild_test_counts(CabM* cabm, CabM* cabm_ref, int num_add_ptcl) {
-  int fails = 0;
-  if (cabm->nElems() != cabm_ref->nElems()) {
-    fprintf(stderr, "[ERROR] Element count mismatch on rank %d "
-            "[(structure)%d != %d(reference)]\n",
-            comm_rank, cabm->nElems(), cabm_ref->nElems());
-    ++fails;
-  }
-  if (cabm->nPtcls() + num_add_ptcl != cabm_ref->nPtcls()) {
-    fprintf(stderr, "[ERROR] Particle count mismatch on rank %d "
-            "[(structure)%d != %d(reference)]\n",
-            comm_rank, cabm->nPtcls(), cabm_ref->nPtcls());
-    ++fails;
-  }
-  if (cabm->numRows() < cabm_ref->nElems()) {
-    fprintf(stderr, "[ERROR] Number of rows is too small to fit elements on rank %d "
-            "[(structure)%d < %d(reference)]\n", comm_rank,
-            cabm->numRows(), cabm_ref->nElems());
-    ++fails;
-  }
-  if (cabm->capacity() < cabm_ref->nPtcls() + num_add_ptcl) {
-    fprintf(stderr, "[ERROR] Capcity is too small to fit particles on rank %d "
-            "[(structure)%d < %d(reference)]\n", comm_rank,
-            cabm->capacity(), cabm_ref->nPtcls());
-    ++fails;
-  }
-  return fails;
-}
-
 //Rebuild test with no changes to structure
 bool rebuildNoChanges(int ne_in, int np_in,int distribution){
   Kokkos::Profiling::pushRegion("rebuildNoChanges");
@@ -126,8 +96,6 @@ bool rebuildNoChanges(int ne_in, int np_in,int distribution){
 
   //Structure to perform rebuild on
   CabM* cabm = new CabM(po, ne, np, ptcls_per_elem_v, element_gids_v);
-  //Structure to compare rebuild result to 
-  CabM* cabm_ref = new CabM(po, ne, np, ptcls_per_elem_v, element_gids_v);
 
   delete [] ptcls_per_elem;
   delete [] ids;
@@ -137,58 +105,65 @@ bool rebuildNoChanges(int ne_in, int np_in,int distribution){
   //pID = particle ID
   //Gives access to MTV data to set an identifier for each ptcl
   auto pID = cabm->get<0>();
-  auto pID_ref = cabm_ref->get<0>();
+
+  //Sum of the particles in each element
+  CabM::kkLidView element_sums("element_sums", cabm->nElems());
 
   //Assign values to ptcls to track their movement
-  auto sendToSelf = PS_LAMBDA(const lid_t& elm, const lid_t& ptcl, const bool& active) {
-    if (active) {
-      new_element(ptcl) = elm;
-      pID(ptcl) = ptcl;
-      pID_ref(ptcl) = ptcl;
+  auto setSameElement = PS_LAMBDA(const lid_t& e, const lid_t& p, const bool& mask) {
+    if (mask) {
+      new_element(p) = e;
+      Kokkos::atomic_add(&(element_sums(e)), p);
     }
     else {
-      new_element(ptcl) = -1;
-      pID(ptcl) = -1;
-      pID_ref(ptcl) = -1;
+      new_element(p) = -1;
     }
+    pID(p) = p;
   };
-  cabm->parallel_for(sendToSelf, "sendToSelf");
+  ps::parallel_for(cabm, setSameElement, "setSameElement");
 
   //Rebuild with no changes (function takes second and third args as optional)
   cabm->rebuild(new_element);
 
-  int count_fails =  rebuild_test_counts(cabm, cabm_ref, 0);
-  if ( count_fails > 0 ) {
-    return count_fails;
-  }
-
   //(Necessary) update of pID
   pID = cabm->get<0>();
 
+  if (cabm->nPtcls() != np) {
+    fprintf(stderr, "[ERROR] CabM does not have the correct number of particles after "
+        "rebuild %d (should be %d)\n", cabm->nPtcls(), np);
+    ++fails;
+  }
+  //Sum of the particles in each element
+  CabM::kkLidView new_element_sums("new_element_sums", cabm->nElems());
+
   kkLidView failed = kkLidView("failed", 1);
-  Kokkos::parallel_for("check no changes", cabm->capacity(),
-      KOKKOS_LAMBDA (const int& ptcl) {
-        const lid_t id = pID_ref(ptcl);
-        if (id != -1) { // only looking at active particles
-          const lid_t dest_elem = new_element(ptcl);
-
-          bool found = false;
-          for (lid_t i = dest_elem*AoSoA::vector_length; i < (dest_elem+1)*AoSoA::vector_length; ++i) {
-            //printf("looking for: %d, index: %d, id: %d\n", id, i, pID(i));
-            if (pID(i) == id) {
-              found = true;
-              break;
-            } 
-          }
-          if (!found) {
-            //printf("[ERROR] particle %d not found in element %d\n", id, dest_elem);
-            failed(0) = 1;
-          }
-        }
-      });
-
+  auto checkSameElement = PS_LAMBDA(const lid_t e, const lid_t p, const bool mask) {
+    const lid_t id = pID(p);
+    const lid_t dest_elem = new_element(id);
+    if (mask) {
+      Kokkos::atomic_add(&(new_element_sums[e]), id);
+      if (dest_elem != e) {
+        printf("[ERROR] Particle %d was moved to incorrect element %d "
+               "(should be in element %d)\n", id, e, dest_elem);
+        failed(0) = 1;
+      }
+    }
+  };
+  ps::parallel_for(cabm, checkSameElement, "checkSameElement");
   fails += getLastValue<lid_t>(failed);
 
+  CabM::kkLidView failed2("failed2", 1);
+  auto checkElementSums = KOKKOS_LAMBDA(const int i) {
+    const lid_t old_sum = element_sums(i);
+    const lid_t new_sum = new_element_sums(i);
+    if (old_sum != new_sum) {
+      printf("Sum of particle ids on element %d do not match. Old: %d New: %d\n",
+             i, old_sum, new_sum);
+      failed2(0) = 1;
+    }
+  };
+  Kokkos::parallel_for(cabm->nElems(), checkElementSums, "checkElementSums");
+  fails += getLastValue<lid_t>(failed);
   Kokkos::Profiling::popRegion();
   return fails;
 }
@@ -203,76 +178,80 @@ bool rebuildNewElems(int ne_in, int np_in,int distribution){
   int np = np_in;
   int* ptcls_per_elem = new int[ne];
   std::vector<int>* ids = new std::vector<int>[ne];
-  distribute_particles(ne, np, distribution, ptcls_per_elem, ids); 
+  distribute_particles(ne, np, distribution, ptcls_per_elem, ids);
   Kokkos::TeamPolicy<exe_space> po(32,Kokkos::AUTO);
   CabM::kkLidView ptcls_per_elem_v("ptcls_per_elem_v",ne);
   CabM::kkGidView element_gids_v("",0);
   particle_structs::hostToDevice(ptcls_per_elem_v,ptcls_per_elem);
 
-  //Structure to perform rebuild on
   CabM* cabm = new CabM(po, ne, np, ptcls_per_elem_v, element_gids_v);
-  //Structure to compare rebuild result to 
-  CabM* cabm_ref = new CabM(po, ne, np, ptcls_per_elem_v, element_gids_v);
 
   delete [] ptcls_per_elem;
   delete [] ids;
 
-  CabM::kkLidView new_element("new_element", cabm->capacity(), 0);
+  CabM::kkLidView new_element("new_element", cabm->capacity());
 
-  //pID = particle ID
-  //Gives access to MTV data to set an identifier for each ptcl
   auto pID = cabm->get<0>();
-  auto pID_ref = cabm_ref->get<0>();
 
-  //Assign values to ptcls to track their movement
-  auto sendToSelf = PS_LAMBDA(const lid_t& elm, const lid_t& ptcl, const bool& active) {
-    if (active) {
-      new_element(ptcl) = (elm*3 + ptcl)%ne; //change to assign to diff elems
-      pID(ptcl) = ptcl;
-      pID_ref(ptcl) = ptcl;
+  //Sum of the particles in each element
+  CabM::kkLidView element_sums("element_sums", cabm->nElems());
+
+  //Assign ptcl identifiers
+  auto setElement = PS_LAMBDA(const lid_t e, const lid_t p, const bool mask) {
+    if (mask) {
+      new_element(p) = (e*3 + p) % ne; //assign to diff elems
+      Kokkos::atomic_add(&(element_sums(new_element(p))), p);
     }
-    else {
-      new_element(ptcl) = -1;
-      pID(ptcl) = -1;
-      pID_ref(ptcl) = -1;
-    }
+    else
+      new_element(p) = -1;
+
+    pID(p) = p;
   };
-  cabm->parallel_for(sendToSelf, "sendToSelf");
+  ps::parallel_for(cabm, setElement, "setElement");
 
-  //Rebuild with no changes (function takes second and third args as optional)
+  //Rebuild with no changes
   cabm->rebuild(new_element);
 
-  int count_fails =  rebuild_test_counts(cabm, cabm_ref, 0);
-  if ( count_fails > 0 ) {
-    return count_fails;
+  if (cabm->nPtcls() != np) {
+    fprintf(stderr, "[ERROR] CabM does not have the correct number of particles after "
+            "rebuild %d (should be %d)\n", cabm->nPtcls(), np);
+    ++fails;
   }
 
   //(Necessary) update of pID
   pID = cabm->get<0>();
 
+  //Sum of the particles in each element
+  CabM::kkLidView new_element_sums("new_element_sums", cabm->nElems());
+
   kkLidView failed = kkLidView("failed", 1);
-  Kokkos::parallel_for("check no changes", cabm->capacity(),
-      KOKKOS_LAMBDA (const int& ptcl) {
-        const lid_t id = pID_ref(ptcl);
-        if (id != -1) { // only looking at active particles
-          const lid_t dest_elem = new_element(ptcl);
-
-          bool found = false;
-          for (lid_t i = dest_elem*AoSoA::vector_length; i < (dest_elem+1)*AoSoA::vector_length; ++i) {
-            //printf("looking for: %d, index: %d, id: %d\n", id, i, pID(i));
-            if (pID(i) == id) {
-              found = true;
-              break;
-            } 
-          }
-          if (!found) {
-            //printf("[ERROR] particle %d not found in element %d\n", id, dest_elem);
-            failed(0) = 1;
-          }
-        }
-      });
-
+  auto checkElement = PS_LAMBDA(const lid_t e, const lid_t p, const bool mask) {
+    const lid_t id = pID(p);
+    const lid_t dest_elem = new_element(id);
+    if (mask) {
+      Kokkos::atomic_add(&(new_element_sums[e]), id);
+      if (dest_elem != e) {
+        printf("[ERROR] Particle %d was moved to incorrect element %d "
+               "(should be in element %d)\n", id, e, dest_elem);
+        failed(0) = 1;
+      }
+    }
+  };
+  ps::parallel_for(cabm, checkElement, "checkElement");
   fails += getLastValue<lid_t>(failed);
+
+    CabM::kkLidView failed2("failed2", 1);
+    auto checkElementSums = KOKKOS_LAMBDA(const int i) {
+      const lid_t old_sum = element_sums(i);
+      const lid_t new_sum = new_element_sums(i);
+      if (old_sum != new_sum) {
+        printf("Sum of particle ids on element %d do not match. Old: %d New: %d\n",
+               i, old_sum, new_sum);
+        failed2(0) = 1;
+      }
+    };
+    Kokkos::parallel_for(cabm->nElems(), checkElementSums, "checkElementSums");
+    fails += getLastValue<lid_t>(failed);
 
   Kokkos::Profiling::popRegion();
   return fails;

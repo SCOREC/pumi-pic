fc6d03eb7dfd1a6fcce6b37a87dc32d24807d96f
MatthewChristoff <matthew.christoff2000@gmail.com>
2021-02-08 13:27:20 -0500

    Added rebuild tests, small passed, all others fail

--- particle_structs/src/cabm/cabm.hpp
+++ particle_structs/src/cabm/cabm.hpp
@@ -335,14 +335,15 @@ namespace pumipic {
                                          kkLidView new_particle_elements,
                                          MTVs new_particles) {
     const auto soa_len = AoSoA_t::vector_length;
-    Kokkos::View<lid_t*> elmDegree("elmDegree", num_elems);
-    Kokkos::View<lid_t*> elmOffsets("elmOffsets", num_elems);
+    kkLidView elmDegree("elmDegree", num_elems);
+    kkLidView elmOffsets("elmOffsets", num_elems);
     const auto activeSliceIdx = aosoa_.number_of_members-1;
     auto active = Cabana::slice<activeSliceIdx>(aosoa_);
-    kkLidView elmDegree_d("elmDegree_device", elmDegree.size());
-    hostToDevice(elmDegree_d,elmDegree.data());
+    auto elmDegree_d = Kokkos::create_mirror_view_and_copy(memory_space(), elmDegree);
+
     //first loop to count number of particles per new element (atomic)
-    auto atomic = KOKKOS_LAMBDA(const int& soa,const int& tuple){
+    assert(new_element.size() == capacity_);
+    auto atomic = KOKKOS_LAMBDA(const int& soa, const int& tuple) {
       if (active.access(soa,tuple) == 1){
         auto parent = new_element((soa*soa_len)+tuple);
         Kokkos::atomic_increment<int>(&elmDegree_d(parent));
@@ -361,17 +362,11 @@ namespace pumipic {
     }
 
     auto elmDegree_h = Kokkos::create_mirror_view_and_copy(host_space(), elmDegree_d);
-
     //prepare a new aosoa to store the shuffled particles
-    auto newOffset = buildOffset(elmDegree_d);
-    const auto newNumSoa = newOffset[num_elems];
+    kkLidView newOffset_d = buildOffset(elmDegree_d);
+    const auto newNumSoa = getLastValue(newOffset_d);
     const auto newCapacity = newNumSoa*soa_len;
     auto newAosoa = makeAoSoA(newCapacity, newNumSoa);
-    //assign the particles from the current aosoa to the newAosoa
-    Kokkos::View<lid_t*,host_space> newOffset_h("newOffset_host",num_elems+1);
-    for (int i=0; i<=num_elems; i++)
-      newOffset_h(i) = newOffset[i];
-    auto newOffset_d = Kokkos::create_mirror_view_and_copy(memory_space(), newOffset_h);
     Kokkos::View<lid_t*, host_space> elmPtclCounter_h("elmPtclCounter_device", num_elems);
     auto elmPtclCounter_d = Kokkos::create_mirror_view_and_copy(memory_space(), elmPtclCounter_h);
     auto newActive = Cabana::slice<activeSliceIdx>(newAosoa);
@@ -390,57 +385,57 @@ namespace pumipic {
           newAosoa.setTuple(firstSoa*soa_len + occupiedTuples, oldTuple);
         }
       };
-      Cabana::simd_parallel_for(simd_policy, copyPtcls, "copyPtcls");
+    Cabana::simd_parallel_for(simd_policy, copyPtcls, "copyPtcls");
 
-      num_soa_ = newNumSoa;
-      capacity_ = newCapacity;
-      offsets = newOffset;
+    num_soa_ = newNumSoa;
+    capacity_ = newCapacity;
+    offsets = newOffset_d;
 
-      // add new particles
-      if (new_particle_elements.size() > 0 && new_particles != NULL) {
-        auto new_num_ptcls = new_particle_elements.size();
+    // add new particles
+    if (new_particle_elements.size() > 0 && new_particles != NULL) {
+      auto new_num_ptcls = new_particle_elements.size();
 
-        // View for tracking particle index in elements
-        kkLidView ptcl_elm_indices("ptcl_elm_indices", num_elems);
-        Kokkos::parallel_for("fill_zeros", num_elems,
-          KOKKOS_LAMBDA(const lid_t& elm) {
-            ptcl_elm_indices(elm) = 0;
-          });
-        // add all CURRENTLY active particles
-        auto fill_elm_indices = PS_LAMBDA(const lid_t& elm, const lid_t& ptcl, const bool& mask) {
-          ptcl_elm_indices(elm) = Kokkos::atomic_fetch_add(&ptcl_elm_indices(elm),mask);
-        };
-        parallel_for( fill_elm_indices, "fill_elm_indices" );
+      // View for tracking particle index in elements
+      kkLidView ptcl_elm_indices("ptcl_elm_indices", num_elems);
+      Kokkos::parallel_for("fill_zeros", num_elems,
+        KOKKOS_LAMBDA(const lid_t& elm) {
+          ptcl_elm_indices(elm) = 0;
+        });
+      // add all CURRENTLY active particles
+      auto fill_elm_indices = PS_LAMBDA(const lid_t& elm, const lid_t& ptcl, const bool& mask) {
+        ptcl_elm_indices(elm) = Kokkos::atomic_fetch_add(&ptcl_elm_indices(elm),mask);
+      };
+      parallel_for( fill_elm_indices, "fill_elm_indices" );
 
-        kkLidView particle_indices("particle_indices", new_num_ptcls);
-        // atomic_fetch_add to increment from the beginning of each element for NEW particles
-        Kokkos::parallel_for("fill_ptcl_indices", new_num_ptcls,
-          KOKKOS_LAMBDA(const lid_t ptcl_id) {
-            particle_indices(ptcl_id) = Kokkos::atomic_fetch_add(&ptcl_elm_indices(new_particle_elements(ptcl_id)),1);
-          });
+      kkLidView particle_indices("particle_indices", new_num_ptcls);
+      // atomic_fetch_add to increment from the beginning of each element for NEW particles
+      Kokkos::parallel_for("fill_ptcl_indices", new_num_ptcls,
+        KOKKOS_LAMBDA(const lid_t ptcl_id) {
+          particle_indices(ptcl_id) = Kokkos::atomic_fetch_add(&ptcl_elm_indices(new_particle_elements(ptcl_id)),1);
+        });
 
-        auto offsets_cpy = offsets; // copy of offsets since GPUs don't like member variables
-        // calculate SoA and ptcl in SoA indices for next CopyMTVsToAoSoA
-        kkLidView soa_indices("soa_indices", new_num_ptcls);
-        kkLidView soa_ptcl_indices("soa_ptcl_indices", new_num_ptcls);
-        Kokkos::parallel_for("soa_and_ptcl", new_num_ptcls,
-          KOKKOS_LAMBDA(const lid_t ptcl_id) {
+      auto offsets_cpy = offsets; // copy of offsets since GPUs don't like member variables
+      // calculate SoA and ptcl in SoA indices for next CopyMTVsToAoSoA
+      kkLidView soa_indices("soa_indices", new_num_ptcls);
+      kkLidView soa_ptcl_indices("soa_ptcl_indices", new_num_ptcls);
+      Kokkos::parallel_for("soa_and_ptcl", new_num_ptcls,
+        KOKKOS_LAMBDA(const lid_t ptcl_id) {
 
-            soa_indices(ptcl_id) = offsets_cpy(new_particle_elements(ptcl_id))
-              + (particle_indices(ptcl_id)/soa_len);
-            soa_ptcl_indices(ptcl_id) = particle_indices(ptcl_id)%soa_len;
+          soa_indices(ptcl_id) = offsets_cpy(new_particle_elements(ptcl_id))
+            + (particle_indices(ptcl_id)/soa_len);
+          soa_ptcl_indices(ptcl_id) = particle_indices(ptcl_id)%soa_len;
 
-          });
-        CopyMTVsToAoSoA<device_type, DataTypes>(aosoa_, new_particles, soa_indices,
-          soa_ptcl_indices);
-      }
+        });
+      CopyMTVsToAoSoA<device_type, DataTypes>(aosoa_, new_particles, soa_indices,
+        soa_ptcl_indices);
+    }
 
-      num_ptcls = num_ptcls + new_particle_elements.size();
-      parentElms_ = getParentElms(num_elems, num_soa_, offsets);
+    num_ptcls = num_ptcls + new_particle_elements.size();
+    parentElms_ = getParentElms(num_elems, num_soa_, offsets);
 
-      //destroy the old aosoa and use the new one in the CabanaM object
-      aosoa_ = newAosoa;
-      setActive(aosoa_, elmDegree_d, parentElms_, offsets);
+    //destroy the old aosoa and use the new one in the CabanaM object
+    aosoa_ = newAosoa;
+    setActive(aosoa_, elmDegree_d, parentElms_, offsets);
   }
 
   /**
--- particle_structs/test/rebuild_cabm.cpp
+++ particle_structs/test/rebuild_cabm.cpp
@@ -14,9 +14,16 @@ int comm_rank, comm_size;
 using particle_structs::MemberTypes;
 using particle_structs::getLastValue;
 using particle_structs::lid_t;
-typedef Kokkos::DefaultExecutionSpace exec_space;
+using AoSoA = Cabana::AoSoA<Cabana::MemberTypes<int>,DeviceSpace::device_type>;
+typedef Kokkos::DefaultExecutionSpace exe_space;
 typedef MemberTypes<int> Type;
 typedef particle_structs::CabM<Type> CabM;
+
+bool rebuildNoChanges(int ne_in, int np_in,int distribution);
+bool rebuildNewElems(int ne_in, int np_in,int distribution);
+bool rebuildNewPtcls(int ne_in, int np_in,int distribution);
+bool rebuildPtclsDestroyed(int ne_in, int np_in,int distribution);
+bool rebuildNewAndDestroyed(int ne_in, int np_in,int distribution);
 
 
 int main(int argc, char* argv[]) {
@@ -44,10 +51,229 @@ int main(int argc, char* argv[]) {
     //Tests to run go here
     ///////////////////////////////////////////////////////////////////////////
 
+    //verify still correct elements and no changes otherwise
+    fprintf(stderr,"RebuildNoChanges\n");
+    fails += rebuildNoChanges(number_elements,number_particles,distribution);
 
+    
+    //verify all elements are correctly assigned to new elements
+    fprintf(stderr,"RebuildNewElems\n");
+    fails += rebuildNewElems(number_elements,number_particles,distribution);
+    /*
+    //check new elements are added properly and all elements assgined correct
+    fprintf(stderr,"RebuildNewPtcls\n");
+    fails += rebuildNewPtcls(number_elements,number_particles,distribution);
+
+    //check for particles that were removed and the rest in their correct loc
+    fprintf(stderr,"RebuildPtclsDestroyed\n");
+    fails += rebuildPtclsDestroyed(number_elements,number_particles,distribution);
+
+    //complete check of rebuild functionality
+    fprintf(stderr,"RebuildNewAndDestroyed\n");
+    fails += rebuildNewAndDestroyed(number_elements,number_particles,distribution);
+    */
   }
   //end Kokkos scope
   Kokkos::finalize();
   MPI_Finalize();
+  return fails;
+}
+
+int rebuild_test_counts(CabM* cabm, CabM* cabm_ref, int num_add_ptcl) {
+  int fails = 0;
+  if (cabm->nElems() != cabm_ref->nElems()) {
+    fprintf(stderr, "[ERROR] Element count mismatch on rank %d "
+            "[(structure)%d != %d(reference)]\n",
+            comm_rank, cabm->nElems(), cabm_ref->nElems());
+    ++fails;
+  }
+  if (cabm->nPtcls() + num_add_ptcl != cabm_ref->nPtcls()) {
+    fprintf(stderr, "[ERROR] Particle count mismatch on rank %d "
+            "[(structure)%d != %d(reference)]\n",
+            comm_rank, cabm->nPtcls(), cabm_ref->nPtcls());
+    ++fails;
+  }
+  if (cabm->numRows() < cabm_ref->nElems()) {
+    fprintf(stderr, "[ERROR] Number of rows is too small to fit elements on rank %d "
+            "[(structure)%d < %d(reference)]\n", comm_rank,
+            cabm->numRows(), cabm_ref->nElems());
+    ++fails;
+  }
+  if (cabm->capacity() < cabm_ref->nPtcls() + num_add_ptcl) {
+    fprintf(stderr, "[ERROR] Capcity is too small to fit particles on rank %d "
+            "[(structure)%d < %d(reference)]\n", comm_rank,
+            cabm->capacity(), cabm_ref->nPtcls());
+    ++fails;
+  }
+  return fails;
+}
+
+//Rebuild test with no changes to structure
+bool rebuildNoChanges(int ne_in, int np_in,int distribution){
+  Kokkos::Profiling::pushRegion("rebuildNoChanges");
+  int fails = 0;
+
+  //Test construction based on SCS testing
+  int ne = ne_in;
+  int np = np_in;
+  int* ptcls_per_elem = new int[ne];
+  std::vector<int>* ids = new std::vector<int>[ne];
+  distribute_particles(ne, np, distribution, ptcls_per_elem, ids); 
+  Kokkos::TeamPolicy<exe_space> po(32,Kokkos::AUTO);
+  CabM::kkLidView ptcls_per_elem_v("ptcls_per_elem_v",ne);
+  CabM::kkGidView element_gids_v("",0);
+  particle_structs::hostToDevice(ptcls_per_elem_v,ptcls_per_elem);
+
+  //Structure to perform rebuild on
+  CabM* cabm = new CabM(po, ne, np, ptcls_per_elem_v, element_gids_v);
+  //Structure to compare rebuild result to 
+  CabM* cabm_ref = new CabM(po, ne, np, ptcls_per_elem_v, element_gids_v);
+
+  delete [] ptcls_per_elem;
+  delete [] ids;
+
+  CabM::kkLidView new_element("new_element", cabm->capacity());
+
+  //pID = particle ID
+  //Gives access to MTV data to set an identifier for each ptcl
+  auto pID = cabm->get<0>();
+  auto pID_ref = cabm_ref->get<0>();
+
+  //Assign values to ptcls to track their movement
+  auto sendToSelf = PS_LAMBDA(const lid_t& elm, const lid_t& ptcl, const bool& active) {
+    if (active) {
+      new_element(ptcl) = elm;
+      pID(ptcl) = ptcl;
+      pID_ref(ptcl) = ptcl;
+    }
+    else {
+      new_element(ptcl) = -1;
+      pID(ptcl) = -1;
+      pID_ref(ptcl) = -1;
+    }
+  };
+  cabm->parallel_for(sendToSelf, "sendToSelf");
+
+  //Rebuild with no changes (function takes second and third args as optional)
+  cabm->rebuild(new_element);
+
+  int count_fails =  rebuild_test_counts(cabm, cabm_ref, 0);
+  if ( count_fails > 0 ) {
+    return count_fails;
+  }
+
+  //(Necessary) update of pID
+  pID = cabm->get<0>();
+
+  kkLidView failed = kkLidView("failed", 1);
+  Kokkos::parallel_for("check no changes", cabm->capacity(),
+      KOKKOS_LAMBDA (const int& ptcl) {
+        const lid_t id = pID_ref(ptcl);
+        if (id != -1) { // only looking at active particles
+          const lid_t dest_elem = new_element(ptcl);
+
+          bool found = false;
+          for (lid_t i = dest_elem*AoSoA::vector_length; i < (dest_elem+1)*AoSoA::vector_length; ++i) {
+            //printf("looking for: %d, index: %d, id: %d\n", id, i, pID(i));
+            if (pID(i) == id) {
+              found = true;
+              break;
+            } 
+          }
+          if (!found) {
+            //printf("[ERROR] particle %d not found in element %d\n", id, dest_elem);
+            failed(0) = 1;
+          }
+        }
+      });
+
+  fails += getLastValue<lid_t>(failed);
+
+  Kokkos::Profiling::popRegion();
+  return fails;
+}
+
+//Rebuild test with no new particles, but reassigned particle elements
+bool rebuildNewElems(int ne_in, int np_in,int distribution){
+  Kokkos::Profiling::pushRegion("rebuildNewElems");
+  int fails = 0;
+
+  //Test construction based on SCS testing
+  int ne = ne_in;
+  int np = np_in;
+  int* ptcls_per_elem = new int[ne];
+  std::vector<int>* ids = new std::vector<int>[ne];
+  distribute_particles(ne, np, distribution, ptcls_per_elem, ids); 
+  Kokkos::TeamPolicy<exe_space> po(32,Kokkos::AUTO);
+  CabM::kkLidView ptcls_per_elem_v("ptcls_per_elem_v",ne);
+  CabM::kkGidView element_gids_v("",0);
+  particle_structs::hostToDevice(ptcls_per_elem_v,ptcls_per_elem);
+
+  //Structure to perform rebuild on
+  CabM* cabm = new CabM(po, ne, np, ptcls_per_elem_v, element_gids_v);
+  //Structure to compare rebuild result to 
+  CabM* cabm_ref = new CabM(po, ne, np, ptcls_per_elem_v, element_gids_v);
+
+  delete [] ptcls_per_elem;
+  delete [] ids;
+
+  CabM::kkLidView new_element("new_element", cabm->capacity(), 0);
+
+  //pID = particle ID
+  //Gives access to MTV data to set an identifier for each ptcl
+  auto pID = cabm->get<0>();
+  auto pID_ref = cabm_ref->get<0>();
+
+  //Assign values to ptcls to track their movement
+  auto sendToSelf = PS_LAMBDA(const lid_t& elm, const lid_t& ptcl, const bool& active) {
+    if (active) {
+      new_element(ptcl) = (elm*3 + ptcl)%ne; //change to assign to diff elems
+      pID(ptcl) = ptcl;
+      pID_ref(ptcl) = ptcl;
+    }
+    else {
+      new_element(ptcl) = -1;
+      pID(ptcl) = -1;
+      pID_ref(ptcl) = -1;
+    }
+  };
+  cabm->parallel_for(sendToSelf, "sendToSelf");
+
+  //Rebuild with no changes (function takes second and third args as optional)
+  cabm->rebuild(new_element);
+
+  int count_fails =  rebuild_test_counts(cabm, cabm_ref, 0);
+  if ( count_fails > 0 ) {
+    return count_fails;
+  }
+
+  //(Necessary) update of pID
+  pID = cabm->get<0>();
+
+  kkLidView failed = kkLidView("failed", 1);
+  Kokkos::parallel_for("check no changes", cabm->capacity(),
+      KOKKOS_LAMBDA (const int& ptcl) {
+        const lid_t id = pID_ref(ptcl);
+        if (id != -1) { // only looking at active particles
+          const lid_t dest_elem = new_element(ptcl);
+
+          bool found = false;
+          for (lid_t i = dest_elem*AoSoA::vector_length; i < (dest_elem+1)*AoSoA::vector_length; ++i) {
+            //printf("looking for: %d, index: %d, id: %d\n", id, i, pID(i));
+            if (pID(i) == id) {
+              found = true;
+              break;
+            } 
+          }
+          if (!found) {
+            //printf("[ERROR] particle %d not found in element %d\n", id, dest_elem);
+            failed(0) = 1;
+          }
+        }
+      });
+
+  fails += getLastValue<lid_t>(failed);
+
+  Kokkos::Profiling::popRegion();
   return fails;
 }

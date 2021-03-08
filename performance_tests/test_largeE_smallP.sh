e34d6b5b3fa606a255698db95e8803e3c1729c13
MatthewChristoff <matthew.christoff2000@gmail.com>
2021-03-08 14:36:42 -0500

    small changes to test calls to use multiple processes for psmigrate268

--- performance_tests/test_largeE_smallP.sh
+++ performance_tests/test_largeE_smallP.sh
@@ -12,7 +12,7 @@ do
       do
         mpirun -np 1 ./ps_combo36 $e $((e*1000)) $distribution -p $percent -n $struct
         #mpirun -np 1 ./ps_combo268 $e $((e*1000)) $distribution -p $percent -n $struct
-        #mpirun -np 1 ./ps_migrate268 $e $((e*1000)) $distribution -p $percent -n $struct #remove CSR to run
+        #mpirun -np 2 ./ps_migrate268 $e $((e*1000)) $distribution -p $percent -n $struct #remove CSR to run
       done
     done
   done

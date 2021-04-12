fd26cd403e5aca034efd85cf9c212dafe692ce06
MatthewChristoff <matthew.christoff2000@gmail.com>
2021-04-12 10:40:07 -0400

    uncomment sparse test call in test_all

--- performance_tests/test_all.sh
+++ performance_tests/test_all.sh
@@ -2,7 +2,6 @@
 python output_convert.py smallE_largeP.txt smallE_largeP_rebuild.dat smallE_largeP_push.dat smallE_largeP_migrate.dat
 echo "test_smallE_largeP DONE"
 
-# comment out for now
-#./test_largeE_smallP.sh &> largeE_smallP.txt
-#python output_convert.py largeE_smallP.txt largeE_smallP_rebuild.dat largeE_smallP_push.dat largeE_smallP_migrate.dat
-#echo "test_largeE_smallP DONE"
+./test_largeE_smallP.sh &> largeE_smallP.txt
+python output_convert.py largeE_smallP.txt largeE_smallP_rebuild.dat largeE_smallP_push.dat largeE_smallP_migrate.dat
+echo "test_largeE_smallP DONE"

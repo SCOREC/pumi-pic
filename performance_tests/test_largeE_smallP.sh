4e675b98c71f8536a67977c36ab16c04de9d1b68
MatthewChristoff <matthew.christoff2000@gmail.com>
2021-04-28 10:10:49 -0400

    performance test bash script update for AiMOS sbatch

--- performance_tests/test_largeE_smallP.sh
+++ performance_tests/test_largeE_smallP.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-# Bash script to run a series of ps_combo tests
+# Bash script to run a series of sparsely-populated ps_combo tests
+
+cd ~/barn/pumipic_CabM/
+source envAimos.sh
 
 # Medium-Sparsity Testing Script for AiMOS: large elm n, small ptcl n
+echo "---------------------AiMOS Medium-Sparsity (largeE_smallP)"
 for e in 10000 15000 20000 25000 30000 35000 40000 45000 50000 55000
 do
   for distribution in 1 2 3
--- performance_tests/test_smallE_largeP.sh
+++ performance_tests/test_smallE_largeP.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-# Bash script to run a series of ps_combo tests
+# Bash script to run a series of densely-populated ps_combo tests
+
+cd ~/barn/pumipic_CabM/
+source envAimos.sh
 
 # Dense Testing Script for AiMOS: small elm n, large ptcl n
+echo "---------------------AiMOS Medium-Sparsity (smallE_largeP)"
 for e in 1000 1500 2000 2500 3000 3500 4000 4500 5000 5500
 do
   for distribution in 1 2 3

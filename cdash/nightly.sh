733b4f0ef1df53043a92a81503b46bfc466dd615
Cameron Smith <smithc11@rpi.edu>
2022-01-31 20:35:23 -0500

    cdash: nightly script

--- cdash/nightly.sh
+++ cdash/nightly.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash
 
 #load system modules
 source /etc/profile.d/modules.sh
@@ -22,22 +22,24 @@ function getname() {
   buildSuffix=${machine}-cuda
   echo "build-${name}-${buildSuffix}"
 }
-export engpar=$root/`getname engpar`/install # This is where engpar will be (or is) installed
-export kk=$root/`getname kokkos`/install   # This is where kokkos will be (or is) installed
-export oh=$root/`getname omegah`/install  # This is where omega_h will be (or is) installed
-export cab=$root/`getname cabana`/install # This is where cabana will be (or is) installed
-export pumipic=$root/`getname pumipic`/install # This is where PumiPIC will be (or is) installed
+export engpar=$root/repos/`getname engpar`/install # This is where engpar will be (or is) installed
+export kk=$root/repos/`getname kokkos`/install   # This is where kokkos will be (or is) installed
+export oh=$root/repos/`getname omegah`/install  # This is where omega_h will be (or is) installed
+export cab=$root/repos/`getname cabana`/install # This is where cabana will be (or is) installed
+export pumipic=$root/repos/`getname pumipic`/install # This is where PumiPIC will be (or is) installed
 export CMAKE_PREFIX_PATH=$engpar:$kk:$kk/lib64/cmake:$oh:$cab:$pumipic:$CMAKE_PREFIX_PATH
-export MPICH_CXX=$root/kokkos/bin/nvcc_wrapper
+export MPICH_CXX=$root/repos/kokkos/bin/nvcc_wrapper
 
+set -x
+set -e
 #kokkos
-cd $root
+cd $root/repos
 #git clone -b 3.4.01 git@github.com:kokkos/kokkos.git
 [ -d $kk ] && rm -rf ${kk%%install}
 mkdir -p $kk
-cd $_/..
-cmake ../kokkos \
-  -DCMAKE_CXX_COMPILER=$root/kokkos/bin/nvcc_wrapper \
+cd ${kk%%install}
+cmake $root/repos/kokkos \
+  -DCMAKE_CXX_COMPILER=$root/repos/kokkos/bin/nvcc_wrapper \
   -DKokkos_ARCH_TURING75=ON \
   -DKokkos_ENABLE_SERIAL=ON \
   -DKokkos_ENABLE_OPENMP=off \
@@ -48,15 +50,15 @@ cmake ../kokkos \
 make -j 24 install
 
 ##engpar
-cd $root
+cd $root/repos
 #git clone git@github.com:SCOREC/EnGPar.git
 cd EnGPar
 git pull
 cd -
 [ -d $engpar ] && rm -rf ${engpar%%install}
 mkdir -p $engpar
-cd $_/..
-cmake ../EnGPar \
+cd ${engpar%%install}
+cmake $root/repos/EnGPar \
   -DCMAKE_INSTALL_PREFIX=$engpar \
   -DCMAKE_C_COMPILER="mpicc" \
   -DCMAKE_CXX_COMPILER="mpicxx" \
@@ -67,7 +69,7 @@ cmake ../EnGPar \
 make install -j8
 
 #omegah
-cd $root
+cd $root/repos
 #git clone git@github.com:SCOREC/omega_h.git
 cd omega_h
 git pull
@@ -75,7 +77,7 @@ cd -
 [ -d $oh ] && rm -rf ${oh%%install}
 mkdir -p $oh
 cd ${oh%%install}
-cmake ../omega_h \
+cmake $root/repos/omega_h \
   -DCMAKE_INSTALL_PREFIX=$oh \
   -DBUILD_SHARED_LIBS=OFF \
   -DOmega_h_USE_Kokkos=ON \
@@ -87,14 +89,18 @@ cmake ../omega_h \
   -DKokkos_PREFIX=$kk/lib64/cmake
 make VERBOSE=1 -j8 install
 #ctest -E warp_test_parallel # see https://github.com/SCOREC/pumi-pic/pull/65#issuecomment-824335130
+set +e
+set +x
 
 d=/lore/cwsmith/nightlyBuilds/pumipic
+cd $d
+#remove old compilation
+[ -d build_pumipic ] && rm -rf build_pumipic/
 cd $d/repos/pumipic
 git pull
 git submodule init
 git submodule update
-#remove old compilation
-[ -d build_pumipic ] && rm -rf build_pumipic/
 
+mpicxx -show
 #run nightly test script
 ctest -VV -D Nightly -S $d/repos/pumipic/cdash/nightly.cmake

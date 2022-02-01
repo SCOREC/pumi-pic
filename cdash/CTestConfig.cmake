ec3c30323b753d1c18fdcd52eb5923ef5a3c1215
Cameron Smith <smithc11@rpi.edu>
2022-01-31 20:26:14 -0500

    cdash: update config script

--- cdash/CTestConfig.cmake
+++ cdash/CTestConfig.cmake
@@ -1,7 +1,9 @@
 set(CTEST_PROJECT_NAME "pumi-pic")
-set(CTEST_NIGHTLY_START_TIME "01:00:00 EST")
+set(CTEST_NIGHTLY_START_TIME "01:00:00 America/New_York")
 
-set(CTEST_DROP_METHOD "http")
+set(CTEST_DROP_METHOD "https")
 set(CTEST_DROP_SITE "my.cdash.org")
 set(CTEST_DROP_LOCATION "/submit.php?project=pumi-pic")
 set(CTEST_DROP_SITE_CDASH TRUE)
+
+set(CTEST_PROJECT_SUBPROJECTS pumipic-master)

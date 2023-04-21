140f72698788bf03936ea081971d73c23d8cc4a0
Angelyr <scardking@gmail.com>
2023-04-21 16:45:22 -0400

    test structure passing

--- support/ViewComm_host.hpp
+++ support/ViewComm_host.hpp
@@ -1,30 +1,47 @@
 /************** Host Communication functions **************/
 template <typename Space> using IsHost =
-  typename std::enable_if<Kokkos::SpaceAccessibility<typename Space::memory_space,
-                                                     Kokkos::HostSpace>::accessible, int>::type;
+typename std::enable_if<Kokkos::SpaceAccessibility<typename Space::memory_space, 
+                                                                  Kokkos::HostSpace>::accessible, int>::type;
 //Send
 template <typename ViewT>
 IsHost<ViewSpace<ViewT> > PS_Comm_Send(ViewT view, int offset, int size,
                                        int dest, int tag, MPI_Comm comm) {
-  int size_per_entry = BaseType<ViewType<ViewT> >::size;
-  return MPI_Send(view.data() + offset, size*size_per_entry,
-                  MpiType<BT<ViewType<ViewT> > >::mpitype(), dest, tag, comm);
+  auto subview = Subview<ViewType<ViewT> >::subview(view, offset, size);
+  auto view_host = deviceToHost(subview);
+  return MPI_Send(view_host.data(), view_host.size(), MpiType<BT<ViewType<ViewT> > >::mpitype(),
+                  dest, tag, comm);
 }
 //Recv
 template <typename ViewT>
 IsHost<ViewSpace<ViewT> > PS_Comm_Recv(ViewT view, int offset, int size,
                                        int sender, int tag, MPI_Comm comm) {
-  int size_per_entry = BaseType<ViewType<ViewT> >::size;
-  return MPI_Recv(view.data() + offset, size*size_per_entry, MpiType<BT<ViewType<ViewT> > >::mpitype(),
-                  sender, tag, comm, MPI_STATUS_IGNORE);
+  ViewT new_view("recv_view", size);
+  typename ViewT::HostMirror view_host = create_mirror_view(new_view);
+  int ret = MPI_Recv(view_host.data(), view_host.size(),
+                     MpiType<BT<ViewType<ViewT> > >::mpitype(),
+                     sender, tag, comm, MPI_STATUS_IGNORE);
+  // Copy received values to device and move it to the proper indices of the view
+  deep_copy(new_view, view_host);
+  Kokkos::parallel_for(
+      size, KOKKOS_LAMBDA(const int &i) {
+        copyViewToView(view, i + offset, new_view, i);
+      });
+  return ret;
 }
 //Isend
 template <typename ViewT>
 IsHost<ViewSpace<ViewT> > PS_Comm_Isend(ViewT view, int offset, int size,
                                         int dest, int tag, MPI_Comm comm, MPI_Request* req) {
-  int size_per_entry = BaseType<ViewType<ViewT> >::size;
-  return MPI_Isend(view.data() + offset, size*size_per_entry, MpiType<BT<ViewType<ViewT> > >::mpitype(),
-                   dest, tag, comm, req);
+  auto subview = Subview<ViewType<ViewT> >::subview(view, offset, size);
+  auto view_host = deviceToHost(subview);
+  int ret = MPI_Isend(view_host.data(), view_host.size(),
+                      MpiType<BT<ViewType<ViewT> > >::mpitype(), dest,
+                      tag, comm, req);
+  // Noop that will keep the view_host around until the lambda is removed
+  get_map()[req] = [=](){
+    (void)view_host;
+  };
+  return ret;
 }
 //Irecv
 template <typename ViewT>
@@ -36,56 +53,94 @@ IsHost<ViewSpace<ViewT> > PS_Comm_Irecv(ViewT view, int offset, int size,
   int ret = MPI_Irecv(view_host.data(), size * size_per_entry,
                       MpiType<BT<ViewType<ViewT> > >::mpitype(),
                       sender, tag, comm, req);
-  Kokkos::parallel_for(size, KOKKOS_LAMBDA(const int& i) {
-    copyViewToView(view,i+offset, new_view, i);
-  });
+  get_map()[req] = [=](){
+    Kokkos::parallel_for(
+        size, KOKKOS_LAMBDA(const int &i) {
+          copyViewToView(view, i + offset, new_view, i);
+        });
+  };
   return ret;
 }
 
 //Wait
 template <typename Space>
 IsHost<Space> PS_Comm_Wait(MPI_Request* req, MPI_Status* stat) {
-  return MPI_Wait(req, stat);
+  int ret = MPI_Wait(req, stat);
+  Irecv_Map::iterator itr = get_map().find(req);
+  if (itr != get_map().end()){
+    (itr->second)();
+    get_map().erase(itr);
+  }
+  return ret;
 }
 
 //Waitall
 template <typename Space>
 IsHost<Space> PS_Comm_Waitall(int num_reqs, MPI_Request* reqs, MPI_Status* stats) {
-  return MPI_Waitall(num_reqs, reqs, stats);
+  int ret = MPI_Waitall(num_reqs, reqs, stats);
+  for (int i = 0; i < num_reqs; ++i){
+    Irecv_Map::iterator itr = get_map().find(reqs + i);
+    if (itr != get_map().end()){
+      (itr->second)();
+      get_map().erase(itr);
+    }
+  }
+  return ret;
 }
 //Alltoall
 template <typename ViewT>
 IsHost<ViewSpace<ViewT> > PS_Comm_Alltoall(ViewT send, int send_size,
                                            ViewT recv, int recv_size,
                                            MPI_Comm comm) {
-  return MPI_Alltoall(send.data(), send_size, MpiType<BT<ViewType<ViewT> > >::mpitype(),
-                      recv.data(), recv_size, MpiType<BT<ViewType<ViewT> > >::mpitype(), comm);
+  typename ViewT::HostMirror send_host = deviceToHost(send);
+  typename ViewT::HostMirror recv_host = create_mirror_view(recv);
+  int ret = MPI_Alltoall(send_host.data(), send_size, MpiType<BT<ViewType<ViewT> > >::mpitype(),
+                         recv_host.data(), recv_size, MpiType<BT<ViewType<ViewT> > >::mpitype(), comm);
+  deep_copy(recv, recv_host);
+  return ret;
 }
 
+//Ialltoall
 template <typename ViewT>
 IsHost<ViewSpace<ViewT> > PS_Comm_Ialltoall(ViewT send, int send_size,
                                             ViewT recv, int recv_size,
-                                            MPI_Comm comm, MPI_Request* request) {
-  return MPI_Ialltoall(send.data(), send_size, MpiType<BT<ViewType<ViewT> > >::mpitype(),
-                      recv.data(), recv_size, MpiType<BT<ViewType<ViewT> > >::mpitype(),
-                      comm, request);
-
+                                            MPI_Comm comm, MPI_Request *request) {
+  typename ViewT::HostMirror send_host = deviceToHost(send);
+  typename ViewT::HostMirror recv_host = create_mirror_view(recv);
+  int ret = MPI_Ialltoall(send_host.data(), send_size,
+                          MpiType<BT<ViewType<ViewT> > >::mpitype(),
+                          recv_host.data(), recv_size,
+                          MpiType<BT<ViewType<ViewT> > >::mpitype(), comm, request);
+  get_map()[request] = [=](){
+    deep_copy(recv, recv_host);
+  };
+  return ret;
 }
 
 //reduce
 template <typename ViewT>
 IsHost<ViewSpace<ViewT> > PS_Comm_Reduce(ViewT send_view, ViewT recv_view, int count,
                                          MPI_Op op, int root, MPI_Comm comm) {
-  return MPI_Reduce(send_view.data(), recv_view.data(), count,
-                    MpiType<BT<ViewType<ViewT> > >::mpitype(),
-                    op, root, comm);
-
+  typename ViewT::HostMirror send_host = deviceToHost(send_view);
+  typename ViewT::HostMirror recv_host = create_mirror_view(recv_view);
+  int ret = MPI_Reduce(send_host.data(), recv_host.data(), count,
+                       MpiType<BT<ViewType<ViewT> > >::mpitype(),
+                       op, root, comm);
+  int comm_rank;
+  MPI_Comm_rank(comm, &comm_rank);
+  if (comm_rank == root)
+    deep_copy(recv_view, recv_host);
+  return ret;
 }
 
 //allreduce
 template <typename ViewT>
 IsHost<ViewSpace<ViewT> > PS_Comm_Allreduce(ViewT send_view, ViewT recv_view, int count,
                                             MPI_Op op, MPI_Comm comm) {
-  return MPI_Allreduce(send_view.data(), recv_view.data(), count,
-                       MpiType<BT<ViewType<ViewT> > >::mpitype(), op, comm);
+  typename ViewT::HostMirror send_host = deviceToHost(send_view);
+  typename ViewT::HostMirror recv_host = create_mirror_view(recv_view);
+  int ret = MPI_Allreduce(send_host.data(), recv_host.data(), count,
+                          MpiType<BT<ViewType<ViewT> > >::mpitype(), op, comm);
+  deep_copy(recv_view, recv_host);
+  return ret;
 }

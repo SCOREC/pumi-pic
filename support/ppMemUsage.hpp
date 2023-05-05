21038a3d382627c7bfea8f3ab99fb77aa0a93083
Angelyr <scardking@gmail.com>
2023-05-05 15:01:10 -0400

    check for mallinfo2

--- support/ppMemUsage.hpp
+++ support/ppMemUsage.hpp
@@ -2,7 +2,7 @@
 
 void hostGetMem(size_t* free, size_t* total) {
   const double M = 1024*1024;
-#if defined(__GNUC__) && defined(PUMI_HAS_MALLINFO2)
+#if defined(__GNUC__) && defined(PUMIPIC_HAS_MALLINFO2)
   struct mallinfo2 meminfo_now = mallinfo2();
   *total = meminfo_now.arena/M;
   *free = meminfo_now.fordblks/M;

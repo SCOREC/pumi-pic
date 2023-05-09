8662e8d8ffc9bf0fea29a3e0876b3e9d34f11a0e
Angelyr <scardking@gmail.com>
2023-05-09 15:54:32 -0400

    destroy test fix

--- particle_structs/src/scs/SCS_migrate.h
+++ particle_structs/src/scs/SCS_migrate.h
@@ -45,10 +45,7 @@ namespace pumipic {
     int num_recv_ranks = dist.isWorld() ? 1 : comm_size - 1;
     MPI_Request* count_recv_requests = new MPI_Request[num_recv_ranks];
     if (dist.isWorld()) {
-      //Note using a blocking Alltoall because the code crashes on Summit when >1024 ranks
-      PS_Comm_Alltoall(num_send_particles, 1, num_recv_particles, 1, dist.mpi_comm());
-      /*PS_Comm_Ialltoall(num_send_particles, 1, num_recv_particles, 1, dist.mpi_comm(), count_recv_requests);
-      */
+      PS_Comm_Ialltoall(num_send_particles, 1, num_recv_particles, 1, dist.mpi_comm(), count_recv_requests);
     }
     else {
       int request_index = 0;
@@ -97,9 +94,7 @@ namespace pumipic {
                                                                     new_process,
                                                                     send_index);
 
-    //Wait until all counts are received Note: disabled on world because of crashing on Summit when >1024 processes
-    if (!dist.isWorld())
-      PS_Comm_Waitall<device_type>(num_recv_ranks, count_recv_requests, MPI_STATUSES_IGNORE);
+    PS_Comm_Waitall<device_type>(num_recv_ranks, count_recv_requests, MPI_STATUSES_IGNORE);
     delete [] count_recv_requests;
 
     //Count the number of processes being sent to and recv from

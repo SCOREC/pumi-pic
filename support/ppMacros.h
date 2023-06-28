806efb68380419b072d33996407d15ec3b3e1cbf
Angelyr <scardking@gmail.com>
2023-06-28 15:32:22 -0400

    ppdevice host

--- support/ppMacros.h
+++ support/ppMacros.h
@@ -2,7 +2,7 @@
 
 #ifdef PP_USE_GPU
 #define PP_INLINE __host__ __device__ inline
-#define PP_DEVICE __device__ inline
+#define PP_DEVICE __host__ __device__ inline
 #define PS_LAMBDA [=] __host__ __device__
 #define PP_DEVICE_VAR __device__
 #else

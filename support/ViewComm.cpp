528204f2cbb0f8c540d0609018cd03e493c8f394
Angelyr <scardking@gmail.com>
2023-07-26 17:37:44 -0400

    cmake gpu aware mpi

--- support/ViewComm.cpp
+++ support/ViewComm.cpp
@@ -10,7 +10,7 @@ namespace pumipic {
 
   //Adapted from https://www.open-mpi.org/faq/?category=runcuda
   bool checkCudaAwareMPI() {
-    printf("Compile time check:\n");
+    printf("Compile time check: ");
 #if defined(MPIX_CUDA_AWARE_SUPPORT) && MPIX_CUDA_AWARE_SUPPORT
     printf("This MPI library has CUDA-aware support.\n", MPIX_CUDA_AWARE_SUPPORT);
     return true;
@@ -21,7 +21,7 @@ namespace pumipic {
     printf("This MPI library cannot determine if there is CUDA-aware support.\n");
 #endif /* MPIX_CUDA_AWARE_SUPPORT */
 
-    printf("Run time check:\n");
+    printf("Run time check: ");
 #if defined(MPIX_CUDA_AWARE_SUPPORT)
     if (1 == MPIX_Query_cuda_support()) {
       printf("This MPI library has CUDA-aware support.\n");
@@ -34,26 +34,19 @@ namespace pumipic {
     return false;
   }
 
-  //Adapted from: https://docs.open-mpi.org/en/v5.0.x/man-openmpi/man3/MPIX_Query_rocm_support.3.html#mpix-query-rocm-support
-  bool checkROCmAwareMPI()
+  bool checkPSGPUAwareMPI()
   {
-    bool happy = false;
-#if defined(OMPI_HAVE_MPI_EXT_ROCM) && OMPI_HAVE_MPI_EXT_ROCM
-    happy = (bool) MPIX_Query_rocm_support();
+#ifdef PS_GPU_AWARE_MPI
+    printf("GPU aware MPI has been enabled.\n");
+    return true;
+#else
+    printf("GPU aware MPI is disabled.\n");
+    return false;
 #endif
-
-    if (happy) {
-      printf("This Open MPI installation has ROCm-aware support.\n");
-      return true;
-    } 
-    else {
-      printf("This Open MPI installation does not have ROCm-aware support.\n");
-      return false;
-    }
   }
 
   bool checkGPUAwareMPI()
   {
-    return checkCudaAwareMPI() || checkROCmAwareMPI();
+    return checkCudaAwareMPI() || checkPSGPUAwareMPI();
   }
 }

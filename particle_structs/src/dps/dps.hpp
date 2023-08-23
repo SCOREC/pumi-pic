5d8d984122e9d504cce1e8fb2061d78b3aa11169
Angelyr <scardking@gmail.com>
2023-08-23 17:36:39 -0400

    DPS printformat()

--- particle_structs/src/dps/dps.hpp
+++ particle_structs/src/dps/dps.hpp
@@ -320,35 +320,25 @@ namespace pumipic {
     num_chars = sprintf(ptr, "%s\n", prefix);
     num_chars += sprintf(ptr+num_chars,"Particle Structures DPS\n");
     num_chars += sprintf(ptr+num_chars,"Number of Elements: %d.\nNumber of SoA: %d.\nNumber of Particles: %d.", num_elems, num_soa_, num_ptcls);
-    buffer[num_chars] = '\0';
-    ss << buffer;
+
+    num_chars += sprintf(ptr+num_chars, "\n  Elements");
+    for (int e = 0; e < num_elems; e++){
+      num_chars += sprintf(ptr+num_chars, " %2d(%2d)", e, element_to_gid_host(e));
+    }
 
     lid_t last_soa = -1;
-    lid_t last_elm = -1;
     for (int i = 0; i < capacity_; i++) {
       lid_t soa = i / soa_len;
-      lid_t elm = parents_host(soa);
       if (last_soa == soa) {
-        num_chars = sprintf(ptr," %d", mask_host(i));
-        buffer[num_chars] = '\0';
-        ss << buffer;
+        num_chars += sprintf(ptr+num_chars," %d", mask_host(i));
       }
       else {
-        if (element_to_gid_host.size() > 0) {
-            num_chars = sprintf(ptr,"\n  Element %2d(%2d) | %d", elm, element_to_gid_host(elm), mask_host(i));
-          buffer[num_chars] = '\0';
-          ss << buffer;
-        }
-        else {
-            num_chars = sprintf(ptr,"\n  Element %2d | %d", elm, mask_host(i));
-          buffer[num_chars] = '\0';
-          ss << buffer;
-        }
+        num_chars += sprintf(ptr+num_chars,"\n  SOA %d | %d", soa, mask_host(i));
       }
-
       last_soa = soa;
-      last_elm = elm;
     }
+    buffer[num_chars] = '\0';
+    ss << buffer;
     ss << "\n";
     std::cout << ss.str();
   }

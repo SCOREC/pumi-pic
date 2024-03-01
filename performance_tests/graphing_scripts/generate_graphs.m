c700c216c26994da6d2192292de725f9d4516bbd
Cameron Smith <smithc11@rpi.edu>
2024-02-29 21:40:17 -0500

    use matrix of all timings

--- performance_tests/graphing_scripts/generate_graphs.m
+++ performance_tests/graphing_scripts/generate_graphs.m
@@ -64,7 +64,6 @@ rebuild_data = readFileDataAndClose(inputFileIds{1});
 push_data = readFileDataAndClose(inputFileIds{2});
 migrate_data = readFileDataAndClose(inputFileIds{3});
 
-
 %% Data Filtering
 
 % find length of graphs
@@ -73,86 +72,6 @@ elms = getElms(rebuild_data, structLen);
 
 allData = {rebuild_data push_data migrate_data};
 mat = getMatrix(allData,NUMFUNC,NUMSTRUCT,NUMDIST,length(elms));
-
-% pull distributions and times
-scs_rebuild = getFunctionTimeForStructure(rebuild_data,SCS);
-csr_rebuild = getFunctionTimeForStructure(rebuild_data,CSR);
-cabm_rebuild = getFunctionTimeForStructure(rebuild_data,CABM);
-dps_rebuild = getFunctionTimeForStructure(rebuild_data,DPS);
-scs_push = getFunctionTimeForStructure(push_data,SCS);
-csr_push = getFunctionTimeForStructure(push_data,CSR);
-cabm_push = getFunctionTimeForStructure(push_data,CABM);
-dps_push = getFunctionTimeForStructure(push_data,DPS);
-scs_migrate = getFunctionTimeForStructure(migrate_data,SCS);
-csr_migrate = getFunctionTimeForStructure(migrate_data,CSR);
-cabm_migrate = getFunctionTimeForStructure(migrate_data,CABM);
-dps_migrate = getFunctionTimeForStructure(migrate_data,DPS);
-
-% Separate data by distribution, {0,1,2,3} = {Evenly,Uniform,Gaussian,Exponential}
-
-% SCS Rebuild
-%scs_rebuild_even = scs_rebuild( scs_rebuild(:,1) == 0, 2);
-
-scs_rebuild_uni = getTime(allData, REBUILD, SCS, UNIFORM)
-scs_rebuild_gauss = getTime(rebuild_data, SCS, GAUSS);
-scs_rebuild_exp = getTime(rebuild_data, SCS, EXPONENTIAL);
-% CSR Rebuild
-%csr_rebuild_even = csr_rebuild( csr_rebuild(:,1) == 0, 2);
-csr_rebuild_uni = csr_rebuild( csr_rebuild(:,1) == 1, 2);
-csr_rebuild_gauss = csr_rebuild( csr_rebuild(:,1) == 2, 2);
-csr_rebuild_exp = csr_rebuild( csr_rebuild(:,1) == 3, 2);
-% CabM Rebuild
-%cabm_rebuild_even = cabm_rebuild( cabm_rebuild(:,1) == 0, 2);
-cabm_rebuild_uni = cabm_rebuild( cabm_rebuild(:,1) == 1, 2);
-cabm_rebuild_gauss = cabm_rebuild( cabm_rebuild(:,1) == 2, 2);
-cabm_rebuild_exp = cabm_rebuild( cabm_rebuild(:,1) == 3, 2);
-% DPS Rebuild
-%dps_rebuild_even = dps_rebuild( dps_rebuild(:,1) == 0, 2);
-dps_rebuild_uni = dps_rebuild( dps_rebuild(:,1) == 1, 2);
-dps_rebuild_gauss = dps_rebuild( dps_rebuild(:,1) == 2, 2);
-dps_rebuild_exp = dps_rebuild( dps_rebuild(:,1) == 3, 2);
-
-% SCS Pseudo-Push
-%scs_push_even = scs_push( scs_push(:,1) == 0, 2);
-scs_push_uni = scs_push( scs_push(:,1) == 1, 2);
-scs_push_gauss = scs_push( scs_push(:,1) == 2, 2);
-scs_push_exp = scs_push( scs_push(:,1) == 3, 2);
-% CSR Pseudo-Push
-%csr_push_even = csr_push( csr_push(:,1) == 0, 2);
-csr_push_uni = csr_push( csr_push(:,1) == 1, 2);
-csr_push_gauss = csr_push( csr_push(:,1) == 2, 2);
-csr_push_exp = csr_push( csr_push(:,1) == 3, 2);
-% CabM Pseudo-Push
-%cabm_push_even = cabm_push( cabm_push(:,1) == 0, 2);
-cabm_push_uni = cabm_push( cabm_push(:,1) == 1, 2);
-cabm_push_gauss = cabm_push( cabm_push(:,1) == 2, 2);
-cabm_push_exp = cabm_push( cabm_push(:,1) == 3, 2);
-% DPS Pseudo-Push
-%dps_push_even = dps_push( dps_push(:,1) == 0, 2);
-dps_push_uni = dps_push( dps_push(:,1) == 1, 2);
-dps_push_gauss = dps_push( dps_push(:,1) == 2, 2);
-dps_push_exp = dps_push( dps_push(:,1) == 3, 2);
-
-% SCS Migrate
-%scs_migrate_even = scs_migrate( scs_migrate(:,1) == 0, 2);
-scs_migrate_uni = scs_migrate( scs_migrate(:,1) == 1, 2);
-scs_migrate_gauss = scs_migrate( scs_migrate(:,1) == 2, 2);
-scs_migrate_exp = scs_migrate( scs_migrate(:,1) == 3, 2);
-% % CSR Migrate
-%csr_migrate_even = csr_migrate( csr_migrate(:,1) == 0, 2);
-csr_migrate_uni = csr_migrate( csr_migrate(:,1) == 1, 2);
-csr_migrate_gauss = csr_migrate( csr_migrate(:,1) == 2, 2);
-csr_migrate_exp = csr_migrate( csr_migrate(:,1) == 3, 2);
-% CabM Migrate
-%cabm_migrate_even = cabm_migrate( cabm_migrate(:,1) == 0, 2);
-cabm_migrate_uni = cabm_migrate( cabm_migrate(:,1) == 1, 2);
-cabm_migrate_gauss = cabm_migrate( cabm_migrate(:,1) == 2, 2);
-cabm_migrate_exp = cabm_migrate( cabm_migrate(:,1) == 3, 2);
-% DPS Migrate
-%dps_migrate_even = dps_migrate( dps_migrate(:,1) == 0, 2);
-dps_migrate_uni = dps_migrate( dps_migrate(:,1) == 1, 2);
-dps_migrate_gauss = dps_migrate( dps_migrate(:,1) == 2, 2);
-dps_migrate_exp = dps_migrate( dps_migrate(:,1) == 3, 2);
 
 %% Graph Generation
 
@@ -173,15 +92,15 @@ dps_length = structLen(DPS);
 % Uniform
 ax2 = nexttile;
 semilogy( ...
-    elms(1:cabm_length), scs_push_uni(1:cabm_length)./cabm_push_uni, 'r:', ...
-    elms(1:cabm_length), scs_rebuild_uni(1:cabm_length)./cabm_rebuild_uni, 'r--', ...
-    elms(1:cabm_length), scs_migrate_uni(1:cabm_length)./cabm_migrate_uni, 'r-.', ...
-    elms(1:csr_length), scs_push_uni(1:csr_length)./csr_push_uni, 'b:', ...
-    elms(1:csr_length), scs_rebuild_uni(1:csr_length)./csr_rebuild_uni, 'b--', ...
-    elms(1:csr_length), scs_migrate_uni(1:csr_length)./csr_migrate_uni, 'b-.', ...
-    elms(1:dps_length), scs_push_uni(1:dps_length)./dps_push_uni, 'g:', ...
-    elms(1:dps_length), scs_rebuild_uni(1:dps_length)./dps_rebuild_uni, 'g--', ...
-    elms(1:dps_length), scs_migrate_uni(1:dps_length)./dps_migrate_uni, 'g-.', ...
+    elms(1:cabm_length), getTimeRatio(mat,PUSH,UNIFORM,SCS+1,CABM+1,cabm_length), 'r:', ...
+    elms(1:cabm_length), getTimeRatio(mat,REBUILD,UNIFORM,SCS+1,CABM+1,cabm_length), 'r--', ...
+    elms(1:cabm_length), getTimeRatio(mat,MIGRATE,UNIFORM,SCS+1,CABM+1,cabm_length), 'r-.', ...
+    elms(1:csr_length), getTimeRatio(mat,PUSH,UNIFORM,SCS+1,CSR+1,csr_length), 'b:', ...
+    elms(1:csr_length), getTimeRatio(mat,REBUILD,UNIFORM,SCS+1,CSR+1,csr_length), 'b--', ...
+    elms(1:csr_length), getTimeRatio(mat,MIGRATE,UNIFORM,SCS+1,CSR+1,csr_length), 'b-.', ...
+    elms(1:dps_length), getTimeRatio(mat,PUSH,UNIFORM,SCS+1,DPS+1,dps_length), 'g:', ...
+    elms(1:dps_length), getTimeRatio(mat,REBUILD,UNIFORM,SCS+1,DPS+1,dps_length), 'g--', ...
+    elms(1:dps_length), getTimeRatio(mat,MIGRATE,UNIFORM,SCS+1,DPS+1,dps_length), 'g-.', ...
     elms, ones(size(elms)), 'k', ...
     'LineWidth', LineWidth );
 ax = gca;
@@ -195,15 +114,15 @@ lim2 = axis;
 % Gaussian
 ax3 = nexttile;
 semilogy( ...
-    elms(1:cabm_length), scs_push_gauss(1:cabm_length)./cabm_push_gauss, 'r:', ...
-    elms(1:cabm_length), scs_rebuild_gauss(1:cabm_length)./cabm_rebuild_gauss, 'r--', ...
-    elms(1:cabm_length), scs_migrate_gauss(1:cabm_length)./cabm_migrate_gauss, 'r-.', ...
-    elms(1:csr_length), scs_push_gauss(1:csr_length)./csr_push_gauss, 'b:', ...
-    elms(1:csr_length), scs_rebuild_gauss(1:csr_length)./csr_rebuild_gauss, 'b--', ...
-    elms(1:csr_length), scs_migrate_gauss(1:csr_length)./csr_migrate_gauss, 'b-.', ...
-    elms(1:dps_length), scs_push_gauss(1:dps_length)./dps_push_gauss, 'g:', ...
-    elms(1:dps_length), scs_rebuild_gauss(1:dps_length)./dps_rebuild_gauss, 'g--', ...
-    elms(1:dps_length), scs_migrate_gauss(1:dps_length)./dps_migrate_gauss, 'g-.', ...
+    elms(1:cabm_length), getTimeRatio(mat,PUSH,GAUSS,SCS+1,CABM+1,cabm_length), 'r:', ...
+    elms(1:cabm_length), getTimeRatio(mat,REBUILD,GAUSS,SCS+1,CABM+1,cabm_length), 'r--', ...
+    elms(1:cabm_length), getTimeRatio(mat,MIGRATE,GAUSS,SCS+1,CABM+1,cabm_length), 'r-.', ...
+    elms(1:csr_length), getTimeRatio(mat,PUSH,GAUSS,SCS+1,CSR+1,csr_length), 'b:', ...
+    elms(1:csr_length), getTimeRatio(mat,REBUILD,GAUSS,SCS+1,CSR+1,csr_length), 'b--', ...
+    elms(1:csr_length), getTimeRatio(mat,MIGRATE,GAUSS,SCS+1,CSR+1,csr_length), 'b-.', ...
+    elms(1:dps_length), getTimeRatio(mat,PUSH,GAUSS,SCS+1,DPS+1,dps_length), 'g:', ...
+    elms(1:dps_length), getTimeRatio(mat,REBUILD,GAUSS,SCS+1,DPS+1,dps_length), 'g--', ...
+    elms(1:dps_length), getTimeRatio(mat,MIGRATE,GAUSS,SCS+1,DPS+1,dps_length), 'g-.', ...
     elms, ones(size(elms)), 'k', ...
     'LineWidth', LineWidth );
 ax = gca;
@@ -217,15 +136,15 @@ lim3 = axis;
 % Exponential
 ax4 = nexttile;
 semilogy( ...
-    elms(1:cabm_length), scs_push_exp(1:cabm_length)./cabm_push_exp, 'r:', ...
-    elms(1:cabm_length), scs_rebuild_exp(1:cabm_length)./cabm_rebuild_exp, 'r--', ...
-    elms(1:cabm_length), scs_migrate_exp(1:cabm_length)./cabm_migrate_exp, 'r-.', ...
-    elms(1:csr_length), scs_push_exp(1:csr_length)./csr_push_exp, 'b:', ...
-    elms(1:csr_length), scs_rebuild_exp(1:csr_length)./csr_rebuild_exp, 'b--', ...
-    elms(1:csr_length), scs_migrate_exp(1:csr_length)./csr_migrate_exp, 'b-.', ...
-    elms(1:dps_length), scs_push_exp(1:dps_length)./dps_push_exp, 'g:', ...
-    elms(1:dps_length), scs_rebuild_exp(1:dps_length)./dps_rebuild_exp, 'g--', ...
-    elms(1:dps_length), scs_migrate_exp(1:dps_length)./dps_migrate_exp, 'g-.', ...
+    elms(1:cabm_length), getTimeRatio(mat,PUSH,EXPONENTIAL,SCS+1,CABM+1,cabm_length), 'r:', ...
+    elms(1:cabm_length), getTimeRatio(mat,REBUILD,EXPONENTIAL,SCS+1,CABM+1,cabm_length), 'r--', ...
+    elms(1:cabm_length), getTimeRatio(mat,MIGRATE,EXPONENTIAL,SCS+1,CABM+1,cabm_length), 'r-.', ...
+    elms(1:csr_length), getTimeRatio(mat,PUSH,EXPONENTIAL,SCS+1,CSR+1,csr_length), 'b:', ...
+    elms(1:csr_length), getTimeRatio(mat,REBUILD,EXPONENTIAL,SCS+1,CSR+1,csr_length), 'b--', ...
+    elms(1:csr_length), getTimeRatio(mat,MIGRATE,EXPONENTIAL,SCS+1,CSR+1,csr_length), 'b-.', ...
+    elms(1:dps_length), getTimeRatio(mat,PUSH,EXPONENTIAL,SCS+1,DPS+1,dps_length), 'g:', ...
+    elms(1:dps_length), getTimeRatio(mat,REBUILD,EXPONENTIAL,SCS+1,DPS+1,dps_length), 'g--', ...
+    elms(1:dps_length), getTimeRatio(mat,MIGRATE,EXPONENTIAL,SCS+1,DPS+1,dps_length), 'g-.', ...
     elms, ones(size(elms)), 'k', ...
     'LineWidth', LineWidth );
 ax = gca;
@@ -250,8 +169,6 @@ axis([ax2 ax3 ax4], limits )
 saveas(f,outname)
 err = 0;
 end
-
-
 
 function data = readFileDataAndClose(fileId)
 data = fscanf(fileId, "%d %d %d %f", [4 Inf])';
@@ -298,13 +215,19 @@ end
 function mat = getMatrix(allData,NUMFUNC,NUMSTRUCT,NUMDIST,MAXELMS)
 mat = zeros(NUMFUNC,NUMSTRUCT,NUMDIST,MAXELMS);
 for func = 1:NUMFUNC
-    for struct = 1:NUMSTRUCT
-        for dist = 1:NUMDIST
+    for struct = 0:NUMSTRUCT-1
+        structIdx = struct+1;
+        for dist = 1:NUMDIST-1 %skipping EVEN, zero based indexing -> don't include end
             time = getTime2(allData, func, struct, dist);
-            time2 = reshape(time, [1,1,1,MAXELMS])
-            mat(func,struct,dist,:) = time2; % FIXME
+            assert( isempty(time) == false );
+            for elm = 1:MAXELMS
+                mat(func,structIdx,dist,elm) = time(elm);
+            end
         end
     end
 end
 end
 
+function ratio = getTimeRatio(mat,func,dist,structA,structB,structB_length)
+ratio = squeeze(mat(func,structA,dist,1:structB_length)./mat(func,structB,dist,:));
+end

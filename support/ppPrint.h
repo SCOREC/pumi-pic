20b1f7550c8159d36d6a905e915e5719395973be
Angelyr <scardking@gmail.com>
2024-11-14 21:29:55 -0500

    optional spdlog

--- support/ppPrint.h
+++ support/ppPrint.h
@@ -1,16 +1,30 @@
 #pragma once
 
-#include "spdlog/spdlog.h"
+#ifdef SPDLOG_ENABLED
+  #include "spdlog/spdlog.h"
+#endif
+
+#include <Kokkos_Core.hpp>
 
 namespace pumipic {
 
   template<typename... Args>
   void pPrintError(const char* fmt, const Args&... args) {
-    spdlog::error(fmt, args...);
+    #ifdef SPDLOG_ENABLED
+      spdlog::error(fmt, args...);
+    #else
+      fprintf(stderr, fmt, args...);
+    #endif
   }
 
   template<typename... Args>
   void pPrintInfo(const char* fmt, const Args&... args) {
-    spdlog::info(fmt, args...);
+    #ifdef SPDLOG_ENABLED
+      spdlog::info(fmt, args...);
+    #else
+      Kokkos::printf(fmt, args...);
+    #endif
   }
+
+
 } 

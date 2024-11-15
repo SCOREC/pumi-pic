a545037cdb2764a7bef61a1567684709c293883e
Angelyr <scardking@gmail.com>
2024-11-15 17:49:06 -0500

    major minor patch version

--- src/pumipic_version.hpp
+++ src/pumipic_version.hpp
@@ -1,7 +1,11 @@
 #pragma once
 
+#define PUMIPIC_VERSION_MAJOR 2
+#define PUMIPIC_VERSION_MINOR 1
+#define PUMIPIC_VERSION_PATCH 4
+
 namespace pumipic {
   const char* pumipic_version() {
-    return "3.26.3";
+    return "2.1.4";
   }
 }

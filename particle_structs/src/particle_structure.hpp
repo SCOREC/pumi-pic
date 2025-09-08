ccd17a6e3fe8fba83d3e2017306641c397c166f7
Sichao25 <sichao_y@outlook.com>
2025-09-08 19:45:05 -0400

    update HostMirrorSpace to host_mirror_space

--- particle_structs/src/particle_structure.hpp
+++ particle_structs/src/particle_structure.hpp
@@ -22,7 +22,7 @@ namespace pumipic {
     typedef typename Space::memory_space memory_space;
     typedef typename Space::execution_space execution_space;
     typedef typename Space::device_type device_type;
-    typedef typename Kokkos::ViewTraits<void, Space>::HostMirrorSpace HostMirrorSpace;
+    typedef typename Kokkos::ViewTraits<void, Space>::host_mirror_space HostMirrorSpace;
     typedef ParticleStructure<DataTypes, HostMirrorSpace> HostMirror;
     template <typename Space2> using Mirror = ParticleStructure<DataTypes, Space2>;
 
